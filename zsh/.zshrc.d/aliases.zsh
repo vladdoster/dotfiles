97462cdb07b6cc93f9fd501dfe9dbfc84b061714
Vlad Doster <mvdoster@gmail.com>
2021-01-04 03:57:34 -0600

    (updates) zsh, nvim, and functions.zsh
    - add coc
    - cleanup cruft
    - zsh history fixed
    - update aliases.zsh
    - format `sh` scripts via shfmt

--- zsh/.zshrc.d/aliases.zsh
+++ zsh/.zshrc.d/aliases.zsh
@@ -1,41 +1,53 @@
 # -- general
-alias ..='cd ..' 
+alias ..='cd ..'
 alias ...='cd ../../../'
 alias ....='cd ../../../../'
 alias .....='cd ../../../../'
 
-alias ll="ls" 
-alias ls='ls -AlhF --color=auto' 
-alias lt='du -sh * | sort -h'
+if [[ $OSTYPE =~ "darwin"   ]]; then
+  alias ll="gls"
+  alias ls='gls -AlhF --color=auto'
+  alias readlink="greadlink"
+  alias sshkey="cat $HOME/.ssh/id_rsa.pub | pbcopy -pboard general && echo 'Copied SSH key to clipboard.'"
+else
+  alias ll="ls"
+  alias ls='ls -AlhF --color=auto'
+  alias sshkey="clip $HOME/.ssh/id_rsa.pub && echo 'Copied SSH key to clipboard.'"
+fi
+
+if [[ $TERM =~ "kitty"   ]]; then
+  alias ssh="kitty +kitten ssh"
+fi
 
 alias cpv='rsync -ah --info=progress2'
+alias lt='du -sh * | sort -h'
 alias mkdir='mkdir -pv'
 alias tailf="less +F -R"
 
 # -- misc.
 alias genpass="openssl rand -base64 24"
 alias myip='curl ifconfig.co'
-alias sshkey="clip $HOME/.ssh/id_rsa.pub && echo 'Copied SSH key to clipboard.'"
+alias shfmt='shfmt -i 2 -ci -sr -kp -s -w'
 alias sz="source $HOME/.zshrc"
 
 # -- webserver debugging
-alias ping='ping -c 10' 
+alias ping='ping -c 10'
 alias ports='netstat -tulanp'       # list open ports
 alias header='curl -I'              # get web server headers
 alias headerc='curl -I --compress'  # does remote server supports gzip / mod_deflate
 
 # -- editor
-alias scratch="vim $(mktemp -t scratch.XXX.md)" 
-alias v.="nvim ." 
-alias v="nvim" 
-alias vi="nvim" 
+alias scratch="vim $(mktemp -t scratch.XXX.md)"
+alias v.="nvim ."
+alias v="nvim"
+alias vi="nvim"
 alias vim="nvim"
 
 # -- python
-alias serve="python -m SimpleHTTPServer" 
+alias serve="python -m SimpleHTTPServer"
 alias va='source ./venv/bin/activate'
-alias ve='python3 -m venv ./venv' 
+alias ve='python3 -m venv ./venv'
 
 # -- git aliases are in ~/.gitconfig.
 alias gcd='cd $(git rev-parse --show-toplevel)'
-alias g='git' 
+alias g='git'
