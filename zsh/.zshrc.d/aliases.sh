64e946e1bb95b8133b421eee7f6dc19d3bed643c
Vladislav Doster <mvdoster@gmail.com>
2021-01-20 21:11:26 -0600

    cleanup

--- zsh/.zshrc.d/aliases.sh
+++ zsh/.zshrc.d/aliases.sh
@@ -5,19 +5,19 @@ alias ....='cd ../../../'
 alias .....='cd ../../../../'
 
 if [[ $OSTYPE =~ "darwin" ]]; then
-	alias ll="gls"
-	alias ls='gls -AlhF --color=auto'
-	alias readlink="greadlink"
-	alias get-public-ssh-key="cat $HOME/.ssh/id_rsa.pub | pbcopy -pboard general && echo 'Copied SSH key to clipboard.'"
+  alias ll="gls"
+  alias ls='gls -AlhF --color=auto'
+  alias readlink="greadlink"
+  alias get-public-ssh-key="cat $HOME/.ssh/id_rsa.pub | pbcopy -pboard general && echo 'Copied SSH key to clipboard.'"
 
-	if [[ $TERM =~ "kitty" ]]; then
-		alias ssh="kitty +kitten ssh"
-	fi
+  if [[ $TERM =~ "kitty" ]]; then
+    alias ssh="kitty +kitten ssh"
+  fi
 
 else
-	alias ll="ls"
-	alias ls="ls -AlhF --color=auto"
-	alias sshkey="clip $HOME/.ssh/id_rsa.pub && echo 'Copied SSH key to clipboard.'"
+  alias ll="ls"
+  alias ls="ls -AlhF --color=auto"
+  alias sshkey="clip $HOME/.ssh/id_rsa.pub && echo 'Copied SSH key to clipboard.'"
 fi
 
 alias cpv="rsync -ah --info=progress2"
@@ -31,19 +31,18 @@ alias get-my-ip="curl ifconfig.co"
 alias reload-zsh="source "${HOME}/.zshrc"
 alias reload-bash="source "${HOME}/.bashrc"
 
-
-function shfmt(){
-    alias SHFMT_OPTS="-i 1 -ci -sr -kp -s -w"
-    shfmt "${SHFMT_OPTS}" 
-    || sudo docker run --rm -v $PWD:/work tmknom/shfmt "${SHFMT_OPTS}" 
-    || echo "Unable to run shfmt" && exit 1
+function shfmt() {
+  alias SHFMT_OPTS="-i 1 -ci -sr -kp -s -w"
+  shfmt "${SHFMT_OPTS}" ||
+    sudo docker run --rm -v $PWD:/work tmknom/shfmt "${SHFMT_OPTS}" ||
+    echo "Unable to run shfmt" && exit 1
 }
 
 # -- webserver debugging
-alias ping='ping -c 10'               # ping 10 times
+alias ping='ping -c 10'                # ping 10 times
 alias get-open-ports='netstat -tulanp' # list open ports
-alias get-website-header='curl -I'    # get web server headers
-alias headerc='curl -I --compress'    # does remote server supports gzip / mod_deflate
+alias get-website-header='curl -I'     # get web server headers
+alias headerc='curl -I --compress'     # does remote server supports gzip / mod_deflate
 
 # -- editor
 alias get-scratchpad="vim $(mktemp -t scratch.XXX.md)"
