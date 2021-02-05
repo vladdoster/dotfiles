1fb40f0d43b3f79f46f246f01ce4a37c3471cf85
vlad doster <mvdoster@gmail.com>
2021-02-05 07:32:06 -0600

    Update functions.zsh

--- zsh/.zshrc.d/functions.zsh
+++ zsh/.zshrc.d/functions.zsh
@@ -1,32 +1,32 @@
-kitty-ssh() {
-  case $TERM in
-  *kitty*) alias ssh="kitty +kitten ssh " ;;
-  *) echo "You arent using the Kitty terminal" && exit 0 ;;
-  esac
+function dcb() {
+  docker-compose exec -it $1 /bin/bash
 }
-
-shell_fmt() {
+function shell_fmt() {
   SHFMT_OPTS="-i 4 -ln bash 1 -ci -sr -kp -s -w"
   sudo docker run --rmi \
     --volume "${PWD}":/work \
     tmknom/shfmt "${SHFMT_OPTS}" \
     "${@}"
 }
-
-set_zsh_as_default_sh() {
+function ssh() {
+  if [[ $TERM =~ "kitty" ]]; then
+    kitty +kitten ssh $@
+  else
+    ssh $@
+  fi
+}
+function set_zsh_as_default_sh() {
   sudo sh --command "echo $(which zsh) >> /etc/shells"
   chsh -s $(which zsh)
   echo "Set zsh as default sh"
 }
-
-remove_ds() {
+function remove_ds() {
   find "${PWD}" \
     -type f \
     -name "*.DS_Store" \
     -ls -delete
 }
-
-add_ssh_host() {
+function add_ssh_host() {
   if [ $# -gt 0 ]; then
     echo "ERROR: add-ssh-host takes no arguments" && exit 1
   else
