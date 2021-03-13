0e6b31710773adaeffee2cff7103304056ab581e
vladdoster <vladdoster@users.noreply.github.com>
2021-03-13 15:13:30 +0000

    Apply shfmt linting changes
    Signed-off-by: GitHub Actions <actions@github.com>

--- scripts/scripts/nuke_docker.sh
+++ scripts/scripts/nuke_docker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker rm --force $(docker ps --all -q) && \
-docker rmi --force $(docker images --all -q) && \
-printf "docker was nuked successfully"
+docker rm --force $(docker ps --all -q) \
+    && docker rmi --force $(docker images --all -q) \
+    && printf "docker was nuked successfully"
