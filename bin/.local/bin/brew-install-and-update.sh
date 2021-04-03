c13f3ee027bd1bc08bbec8b59050d2c18e283185
Vladislav Doster <mvdoster@gmail.com>
2021-04-03 10:35:15 -0500

    (scripts) remove redundant scripts
    - scripts related to brew
    
    Signed-off-by: Vladislav Doster <mvdoster@gmail.com>

--- bin/.local/bin/brew-install-and-update.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "---Installing $1" \
-    && brew install $@ \
-    && echo "---Adding $1 to Brewfile" \
-    && brew bundle dump --all --describe --force --no-lock --debug \
-    && exit 0 \
-    || echo "---WARNING: Brew file not updated" \
-    && exit 1
--- bin/.local/bin/brew-uninstall-and-update.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "---Uninstalling $1" \
-    && brew uninstall $@ \
-    && echo "---Adding $1 to Brewfile" \
-    && brew bundle dump --all --describe --force --no-lock --debug \
-    && exit 0 \
-    || echo "---WARNING: Brew file not updated" \
-    && exit 1
