a556f199d2aff8bef1543fe055bc7f2bdab6235b
vladdoster <vladdoster@users.noreply.github.com>
2021-04-07 22:28:02 +0000

    (linting) apply shfmt fixes
    Signed-off-by: GitHub Actions <actions@github.com>

--- bin/.local/bin/nuke-docker.sh
+++ bin/.local/bin/nuke-docker.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-docker rm -f -v $(docker ps -aq);
-docker rmi -f  $(docker images -q);
-docker volume rm $(docker volume ls -q);
-docker network rm $(docker network ls -q);
+docker rm -f -v $(docker ps -aq)
+docker rmi -f $(docker images -q)
+docker volume rm $(docker volume ls -q)
+docker network rm $(docker network ls -q)
