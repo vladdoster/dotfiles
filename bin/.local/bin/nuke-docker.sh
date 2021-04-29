cea9adb7e6fc087a6a8bae5c4e8e6d3e73aadc33
vlad doster <mvdoster@gmail.com>
2021-04-29 15:54:37 -0500

    Update nuke-docker.sh

--- bin/.local/bin/nuke-docker.sh
+++ bin/.local/bin/nuke-docker.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-docker rm -f -v $(docker ps -aq)
-docker rmi -f $(docker images -q)
-docker volume rm $(docker volume ls -q)
-docker network rm $(docker network ls -q)
+docker rm -f -v $(docker ps -aq) || echo "--- Nothing to remove"
+docker rmi -f $(docker images -q) || echo "--- No images to remove"
+docker volume rm $(docker volume ls -q) || echo "--- No volumes to remove"
+docker network rm $(docker network ls -q) || echo "--- No networks to remove"
+docker system prune --all --force || echo "--- System pruning failed"
