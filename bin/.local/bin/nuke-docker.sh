837304d482146b2e8d9961c2f4a45d1e85cdaeef
vladdoster <vladdoster@users.noreply.github.com>
2021-05-15 20:28:40 +0000

    (linting) apply shfmt fixes
    Signed-off-by: GitHub Actions <actions@github.com>

--- bin/.local/bin/nuke-docker.sh
+++ bin/.local/bin/nuke-docker.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-docker rm -f -v $(docker ps -aq) >/dev/null || echo "--- Nothing to remove"
-docker rmi -f $(docker images -q) >/dev/null || echo "--- No images to remove"
-docker volume rm $(docker volume ls -q) >/dev/null || echo "--- No volumes to remove"
-docker network rm $(docker network ls -q) >/dev/null || echo "--- No networks to remove"
-docker system prune --all --force >/dev/null || echo "--- System pruning failed"
+docker rm -f -v $(docker ps -aq) > /dev/null || echo "--- Nothing to remove"
+docker rmi -f $(docker images -q) > /dev/null || echo "--- No images to remove"
+docker volume rm $(docker volume ls -q) > /dev/null || echo "--- No volumes to remove"
+docker network rm $(docker network ls -q) > /dev/null || echo "--- No networks to remove"
+docker system prune --all --force > /dev/null || echo "--- System pruning failed"
