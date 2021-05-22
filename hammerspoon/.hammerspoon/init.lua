f32de9f40d6667547f6f4cdb9e1e891a01941091
Vladislav Doster <mvdoster@gmail.com>
2021-05-21 22:00:45 -0500

    (maint) hammerspoon config expanded
    Signed-off-by: Vladislav Doster <mvdoster@gmail.com>

--- hammerspoon/.hammerspoon/init.lua
+++ hammerspoon/.hammerspoon/init.lua
@@ -1,9 +1,129 @@
+-- Hammerspoon configuration, heavily influenced by sdegutis default configuration
 
-hs.logger.defaultLogLevel="info"
+--require "bar"
+require "homebrew"
 
-hyper       = {"cmd","alt","ctrl"}
-shift_hyper = {"cmd","alt","ctrl","shift"}
-ctrl_cmd    = {"cmd","ctrl"}
+-- init grid
+hs.grid.MARGINX 	= 0
+hs.grid.MARGINY 	= 0
+hs.grid.GRIDWIDTH 	= 7
+hs.grid.GRIDHEIGHT 	= 3
 
-hs.loadSpoon("Lunette")
-spoon.Lunette:bindHotkeys()
+-- disable animation
+hs.window.animationDuration = 0
+
+-- hotkey mash
+local mash       = {"ctrl", "alt"}
+local mash_app 	 = {"cmd", "alt", "ctrl"}
+local mash_shift = {"ctrl", "alt", "shift"}
+local mash_test	 = {"cntrl", "shift"}
+
+--------------------------------------------------------------------------------
+local appCuts = {
+  c = 'Google Chrome',
+  t = 'kitty',
+  s = 'spotify'
+}
+
+-- Display Help
+local function display_help()
+  local t = {}
+  str = "Keyboard shortcuts\n"
+  table.insert(t str)
+  str = "--------------------\n"
+  table.insert(t, str)
+  for key, app in pairs(appCuts) do
+    str = "^-Alt-Meta-"..key.."\t :\t"..app.."\n"
+    --hs.alert.show(str)
+    table.insert(t,str)
+  end
+  t = table.concat(t)
+  hs.alert.show(t, 2)
+end
+
+-- snap all newly launched windows
+local function auto_tile(appName, event)
+	if event == hs.application.watcher.launched then
+		local app = hs.appfinder.appFromName(appName)
+		-- protect against unexpected restarting windows
+		if app == nil then
+			return
+		end
+		hs.fnutils.map(app:allWindows(), hs.grid.snap)
+	end
+end
+
+-- Moves all windows outside the view into the curent view
+local function rescue_windows()
+    local screen = hs.screen.mainScreen()
+    local screenFrame = screen:fullFrame()
+    local wins = hs.window.visibleWindows()
+    for i,win in ipairs(wins) do
+        local frame = win:frame()
+        if not frame:inside(screenFrame) then
+            win:moveToScreen(screen, true, true)
+        end
+    end
+end
+
+local function init_wm_binding()
+	hs.hotkey.bind(mash_app, '/', function() display_help() end)
+
+	-- global operations
+	hs.hotkey.bind(mash, ';', function() hs.grid.snap(hs.window.focusedWindow()) end)
+	hs.hotkey.bind(mash, "'", function() hs.fnutils.map(hs.window.visibleWindows(), hs.grid.snap) end)
+
+	-- adjust grid size
+	hs.hotkey.bind(mash, '=', function() hs.grid.adjustWidth( 1) end)
+	hs.hotkey.bind(mash, '-', function() hs.grid.adjustWidth(-1) end)
+	hs.hotkey.bind(mash, ']', function() hs.grid.adjustHeight( 1) end)
+	hs.hotkey.bind(mash, '[', function() hs.grid.adjustHeight(-1) end)
+
+	-- change focus
+	hs.hotkey.bind(mash_shift, 'H', function() hs.window.focusedWindow():focusWindowWest() end)
+	hs.hotkey.bind(mash_shift, 'L', function() hs.window.focusedWindow():focusWindowEast() end)
+	hs.hotkey.bind(mash_shift, 'K', function() hs.window.focusedWindow():focusWindowNorth() end)
+	hs.hotkey.bind(mash_shift, 'J', function() hs.window.focusedWindow():focusWindowSouth() end)
+
+	hs.hotkey.bind(mash, 'M', hs.grid.maximizeWindow)
+
+	-- multi monitor
+	hs.hotkey.bind(mash, 'N', hs.grid.pushWindowNextScreen)
+	hs.hotkey.bind(mash, 'P', hs.grid.pushWindowPrevScreen)
+
+	-- move windows
+	hs.hotkey.bind(mash, 'H', hs.grid.pushWindowLeft)
+	hs.hotkey.bind(mash, 'J', hs.grid.pushWindowDown)
+	hs.hotkey.bind(mash, 'K', hs.grid.pushWindowUp)
+	hs.hotkey.bind(mash, 'L', hs.grid.pushWindowRight)
+	hs.hotkey.bind(mash, 'R', function() rescue_windows() end)
+
+	-- resize windows
+	hs.hotkey.bind(mash, 'Y', hs.grid.resizeWindowThinner)
+	hs.hotkey.bind(mash, 'U', hs.grid.resizeWindowShorter)
+	hs.hotkey.bind(mash, 'I', hs.grid.resizeWindowTaller)
+	hs.hotkey.bind(mash, 'O', hs.grid.resizeWindowWider)
+
+	-- Window Hints
+	-- hs.hotkey.bind(mash, '.', function() hs.hints.windowHints(hs.window.allWindows()) end)
+	hs.hotkey.bind(mash, '.', hs.hints.windowHints)
+
+end
+
+-- Init Launch applications bindings
+local function init_app_binding()
+	for key, app in pairs(appCuts) do
+	  hs.hotkey.bind(mash_app, key, function () hs.application.launchOrFocus(app) end)
+	end
+end
+
+local function init()
+	-- Load Hammerspoon bits from https://github.com/jasonrudolph/ControlEscape.spoon
+	hs.loadSpoon('ControlEscape'):start()
+	init_app_binding()
+	init_wm_binding()
+	-- start app launch watcher
+	hs.application.watcher.new(auto_tile):start()
+end
+
+init()
