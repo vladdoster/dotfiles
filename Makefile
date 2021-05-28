7bf411f457160746c545cef0d36f3c202da3be04
Vladislav Doster <mvdoster@gmail.com>
2021-05-27 23:09:02 -0500

    (maint) hammerspoon, zsh, and Makefile
    - Remove Spoons
    - Update aliases because zshrc.d is not hidden
    - Make targets remove git submodules
    
    Signed-off-by: Vladislav Doster <mvdoster@gmail.com>

--- Makefile
+++ Makefile
@@ -13,10 +13,12 @@ list:
 
 install:
 	$(run-stow)
-	echo "--installed dotfiles"
+	git submodule update --init --recursive || "$(echo "failed to install dotfiles" && exit 1)"
+	echo "--installed dotfiles" 
 
 clean : --delete
 	find * -type d -not -path '*/\.*' -exec stow --target="$$HOME" --verbose 1 --delete {} \;	
+	git submodule deinit --all --force || "$(echo "failed to uninstall dotfiles" && exit 1)"
 
 update : --restow
 	$(run-stow)
--- zsh/.config/zsh/.zshrc.d/aliases.sh
+++ zsh/.config/zsh/.zshrc.d/aliases.sh
@@ -4,7 +4,7 @@ if [[ $OSTYPE =~ "darwin" ]]; then
     alias ll="gls"
     alias ls='gls -AlhF --color=auto'
     alias readlink="greadlink"
-    alias get-public-ssh-key="cat ${HOME}/.ssh/id_rsa.pub | pbcopy -pboard general && echo 'Copied SSH key to clipboard.'"
+#    alias get-public-ssh-key="cat ${HOME}/.ssh/id_rsa.pub | pbcopy -pboard general && echo 'Copied SSH key to clipboard.'"
 else
     alias ll="ls"
     alias ls="ls -AlhF --color=auto"
@@ -34,6 +34,7 @@ alias vim="$EDITOR"
 # CONFIG SHORTCUTS
 alias e-aliases="$EDITOR $ZDOTDIR/zshrc.d/aliases.sh"
 alias e-cli-commands="$EDITOR $ZDOTDIR/zshrc.d/cli-commands.sh"
+alias e-hspoon="$EDITOR $HOME/.hammerspoon/init.lua"
 alias e-tmux="$EDITOR $XDG_CONFIG_HOME/tmux/tmux.conf"
 alias e-vimrc="$EDITOR $XDG_CONFIG_HOME/nvim/init.lua"
 alias e-zshrc="$EDITOR $ZDOTDIR/.zshrc"
@@ -47,7 +48,7 @@ alias downloads="cd $HOME/Downloads && ls"
 alias r-black="find . -maxdepth 1 -type f -name  '*.py' -print -exec python3 -m black --line-length=120 --target-version py38 {} \;"
 alias r-shellharden="find . -maxdepth 1 -type f -name '*.sh' -print -exec shellharden --replace {} \;"
 alias r-shfix="r-shellharden && r-shfmt"
-alias r-shfmt="find . -type f -name '*.sh' -print -exec shfmt -i 4 -s -ln bash -sr -bn -ci -w {} \;"
+alias r-shfmt="shfmt -i 4 -s -ln bash -sr -bn -ci -w"
 # GIT ALIASES ARE IN ~/.GITCONFIG.
 alias g='git'
 alias rr='cd "$(git rev-parse --show-toplevel)" && ls'
@@ -57,7 +58,7 @@ alias generate-passwd="openssl rand -base64 24"
 alias get-my-ip="curl ifconfig.co"
 alias reload-sh="exec $SHELL"
 # PYTHON
-alias start-http_server="python -m SimpleHTTPServer"
+alias start-http_server="python2 -m SimpleHTTPServer"
 alias venv-activate="source ./.venv/bin/activate"
 alias venv-create="python3 -m venv ./.venv"
 # WEBSERVER DEBUGGING
@@ -66,8 +67,10 @@ alias get-site-headers='curl -I'       # get web server headers
 alias headerc='curl -I --compress'     # does remote server supports gzip / mod_deflate
 alias ping='ping -c 10'                # ping 10 times
 # DISPLAYS
-alias fix-dual-displays="display-fixer 'id:A49C2C45-F295-3FAE-B81F-247EB03CA52B res:2560x1440 hz:75 color_depth:8 scaling:off origin:(0,0) degree:0' 'id:D614B2F2-B098-8A35-508D-7CEA39295FC9 res:2560x1440 hz:75 color_depth:8 scaling:off origin:(2560,0) degree:0'"
-alias ssh="kitty +kitten ssh"
+alias fix-dual-displays="display-fixer \
+				'id:A49C2C45-F295-3FAE-B81F-247EB03CA52B res:2560x1440 hz:75 color_depth:8 scaling:off origin:(0,0) degree:0'
+				'id:D614B2F2-B098-8A35-508D-7CEA39295FC9 res:2560x1440 hz:75 color_depth:8 scaling:off origin:(2560,0) degree:0'"
+#alias ssh="kitty +kitten ssh"
 # DOCKER
-alias dcd='docker-compose -f local.yml down --remove-orphans --rmi all --volumes'
-alias dcu='docker-compose -f local.yml up'
+#alias dcd='docker-compose -f local.yml down --remove-orphans --rmi all --volumes'
+#alias dcu='docker-compose -f local.yml up'
