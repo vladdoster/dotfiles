56ea274bf7cd203333d1c096f9976c9e6658dc01
vlad doster <mvdoster@gmail.com>
2021-06-01 05:11:28 -0500

    Update init.lua

--- hammerspoon/.hammerspoon/init.lua
+++ hammerspoon/.hammerspoon/init.lua
@@ -10,66 +10,58 @@ require('hs.hotkey').setLogLevel("warning")
 
 -- global config
 config = {
-  apps = {
-    terms    = { 'kitty'                   },
-    browsers = { 'Google Chrome', 'Safari' }
-  },
+    apps = {terms = {'kitty'}, browsers = {'Google Chrome'}},
+    wm = {
+        defaultDisplayLayouts = {
+            ['Color LCD'] = 'main-right',
+            ['LG QHD'] = 'main-right'
 
-  wm = {
-    defaultDisplayLayouts = {
-      ['Color LCD']    = 'monocle',
+        },
+        displayLayouts = {
+            ['Color LCD'] = {'main-right', 'main-center'},
+            ['LG QHD'] = {'main-right', 'main-center'}
+        }
     },
-
-    displayLayouts = {
-      ['Color LCD']    = { 'monocle', 'main-right', 'side-by-side'     },
-    }
-  },
-
-  window = {
-    highlightBorder = false,
-    highlightMouse  = true,
-    historyLimit    = 0
-  },
-
-  network = {
-    home = 'Skynet 5G'
-  },
+    window = {highlightBorder = true, highlightMouse = true, historyLimit = 10},
+    network = {home = 'Moon'}
 }
 
 -- requires
-bindings                    = require('bindings')
-watchables                  = require('utils.watchables')
-watchers                    = require('utils.watchers')
-wm                          = require('utils.wm')
+bindings = require('bindings')
+watchables = require('utils.watchables')
+watchers = require('utils.watchers')
+wm = require('utils.wm')
 
 -- no animations
 hs.window.animationDuration = 0.0
 
 -- hints
-hs.hints.fontName           = 'Helvetica-Bold'
-hs.hints.fontSize           = 22
-hs.hints.hintChars          = { 'A', 'S', 'D', 'F', 'J', 'K', 'L', 'Q', 'W', 'E', 'R', 'Z', 'X', 'C' }
-hs.hints.iconAlpha          = 1.0
-hs.hints.showTitleThresh    = 0
+hs.hints.fontName = 'Helvetica-Bold'
+hs.hints.fontSize = 22
+hs.hints.hintChars = {
+    'A', 'S', 'D', 'F', 'J', 'K', 'L', 'Q', 'W', 'E', 'R', 'Z', 'X', 'C'
+}
+hs.hints.iconAlpha = 1.0
+hs.hints.showTitleThresh = 0
 
 -- watchers
-watchers.enabled            = { 'urlevent' }
-watchers.urlPreference      = config.apps.browsers
+watchers.enabled = {'urlevent'}
+watchers.urlPreference = config.apps.browsers
 
 -- bindings
-bindings.enabled            = { 'ask-before-quit', 'block-hide', 'ctrl-esc', 'f-keys', 'focus', 'global', 'tiling', 'term-ctrl-i', 'viscosity' }
-bindings.askBeforeQuitApps  = config.apps.browsers
+bindings.enabled = {
+    'ask-before-quit', 'block-hide', 'ctrl-esc', 'f-keys', 'focus', 'global',
+    'tiling', 'term-ctrl-i'
+}
+bindings.askBeforeQuitApps = config.apps.browsers
 
 -- start/stop modules
-local modules               = { bindings, watchables, watchers, wm }
+local modules = {bindings, watchables, watchers, wm}
 
-hs.fnutils.each(modules, function(module)
-  if module then module.start() end
-end)
+hs.fnutils.each(modules, function(module) if module then module.start() end end)
 
 -- stop modules on shutdown
 hs.shutdownCallback = function()
-  hs.fnutils.each(modules, function(module)
-    if module then module.stop() end
-  end)
+    hs.fnutils.each(modules,
+                    function(module) if module then module.stop() end end)
 end
