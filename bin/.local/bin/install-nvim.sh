b6966173ac02f43881a5c54af3b141b0795a3ebc
vladdoster <vladdoster@users.noreply.github.com>
2021-06-07 18:12:54 +0000

    (linting) apply shfmt fixes
    Signed-off-by: GitHub Actions <actions@github.com>

--- bin/.local/bin/install-nvim.sh
+++ bin/.local/bin/install-nvim.sh
@@ -1,22 +1,22 @@
-#!/usr/bin/env bash    
+#!/usr/bin/env bash
 
 # the directory of the script
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # the temp directory used, within $DIR
 # omit the -p parameter to create a temporal directory in the default location
 WORK_DIR=$(mktemp -d)
 
 # check if tmp dir was created
-if [[ ! "$WORK_DIR" || ! -d "$WORK_DIR" ]]; then
-  echo "Could not create temp dir"
-  exit 1
+if [[ ! $WORK_DIR || ! -d $WORK_DIR ]]; then
+    echo "Could not create temp dir"
+    exit 1
 fi
 
 # deletes the temp directory
-function cleanup {      
-  rm -rf "$WORK_DIR"
-  echo "Deleted temp working directory $WORK_DIR"
+function cleanup {
+    rm -rf "$WORK_DIR"
+    echo "Deleted temp working directory $WORK_DIR"
 }
 
 # register the cleanup function to be called on the EXIT signal
@@ -30,7 +30,7 @@ echo "--- cloning neovim"
 git clone https://github.com/neovim/neovim
 cd neovim
 echo "--- compiling neovim"
-make CMAKE_BUILD_TYPE=Release 
+make CMAKE_BUILD_TYPE=Release
 echo "--- installing neovim"
 sudo make install
 popd
--- nvim/.config/nvim/install.sh
+++ nvim/.config/nvim/install.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
 get_platform() {
-  case "$OSTYPE" in
-  Linux*) platform=Linux ;;
-  Darwin*) platform=Mac ;;
-  CYGWIN*) platform=Cygwin ;;
-  MINGW*) platform=MinGw ;;
-  *) platform="UNKNOWN:${unameOut}" ;;
-  esac
-  echo "--- Platform: $platform"
+    case "$OSTYPE" in
+        Linux*) platform=Linux ;;
+        Darwin*) platform=Mac ;;
+        CYGWIN*) platform=Cygwin ;;
+        MINGW*) platform=MinGw ;;
+        *) platform="UNKNOWN:${unameOut}" ;;
+    esac
+    echo "--- Platform: $platform"
 }
 
 echo "--- Removing neovim cruft"
@@ -17,25 +17,25 @@ rm -rf ~/.local/share/nvim
 echo "--- Installing packer"
 
 if [ ! -d ~/.local/share/nvim/site/pack/packer ]; then
-  echo "--- Installing packer"
-  git clone https://github.com/wbthomason/packer.nvim \
-    ~/.local/share/nvim/site/pack/packer/start/packer.nvim
-  echo "--- Packer installed!"
+    echo "--- Installing packer"
+    git clone https://github.com/wbthomason/packer.nvim \
+        ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+    echo "--- Packer installed!"
 fi
 
 echo "--- Linking config"
 echo "--- Old nvim config will be deleted"
 rm -rf ~/.config/nvim/ \
-  && mkdir -p ~/.config/nvim \
-  && cp -r init.lua ~/.config/nvim \
-  && cp -r lua ~/.config/nvim
+    && mkdir -p ~/.config/nvim \
+    && cp -r init.lua ~/.config/nvim \
+    && cp -r lua ~/.config/nvim
 
 # change shell in nvim config
 if [ "$(get_platform)" = "Mac" ]; then
-  gsed -i "s/bash/$SHELL/g" ~/.config/nvim/lua/mappings.lua
+    gsed -i "s/bash/$SHELL/g" ~/.config/nvim/lua/mappings.lua
 else
-  sed -i "s/bash/$SHELL/g" ~/.config/nvim/lua/mappings.lua
+    sed -i "s/bash/$SHELL/g" ~/.config/nvim/lua/mappings.lua
 fi
 echo "--- Set shell to $SHELL in nvim successfully!"
 
-nvim +PackerInstall 
+nvim +PackerInstall
