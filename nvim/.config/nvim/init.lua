6fdbb5f644e959ad5dfdfee4236f0ed3df53866b
Vladislav Doster <mvdoster@gmail.com>
2021-06-13 04:57:55 -0500

    (maint) updates
    Signed-off-by: Vladislav Doster <mvdoster@gmail.com>

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,3 +1,6 @@
+
+
+
 local cmd = vim.cmd
 local execute = vim.api.nvim_command
 local fn = vim.fn
@@ -5,9 +8,15 @@ local g = vim.g
 
 g.auto_save = 0
 g.mapleader = " "
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+
+if fn.empty(fn.glob(install_path)) > 0 then
+  fn.system({'git', 'clone', 'https://github.com/wbthomason/packer.nvim', install_path})
+  execute 'packadd packer.nvim'
+end
 -- load all plugins
 require "plugins"
-require "settings"
+require "misc-utils"
 require "user-interface"
 -- lsp stuff
 require "file-finder"
