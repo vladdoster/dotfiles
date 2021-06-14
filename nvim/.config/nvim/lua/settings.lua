789d85c33fc4d647f18f69423a32fa7d1af61e3a
Vladislav Doster <mvdoster@gmail.com>
2021-06-14 11:37:39 -0500

    (maint) remove deprecated neovim settings
    Signed-off-by: Vladislav Doster <mvdoster@gmail.com>

--- nvim/.config/nvim/lua/settings.lua
+++ nvim/.config/nvim/lua/settings.lua
@@ -1,13 +1,17 @@
-vim.o.t_Co = "256" -- Support 256 colors
-TERMINAL = vim.fn.expand('$TERMINAL')
-vim.bo.smartindent = true -- Makes indenting smart
-vim.cmd('filetype plugin on') -- filetype detection
-vim.cmd('let &titleold="'..TERMINAL..'"')
-vim.cmd('set colorcolumn=99999') -- fix indentline for now
-vim.cmd('set expandtab') -- Converts tabs to spaces
-vim.cmd('set inccommand=split') -- Make substitution work in realtime
 vim.cmd('set iskeyword+=-') -- treat dash separated words as a word text object"
 vim.cmd('set shortmess+=c') -- Don't pass messages to |ins-completion-menu|.
+vim.cmd('set inccommand=split') -- Make substitution work in realtime
+
+vim.o.title = true
+TERMINAL = vim.fn.expand('$TERMINAL')
+vim.cmd('let &titleold="'..TERMINAL..'"')
+vim.o.titlestring="%<%F%=%l/%L - nvim"
+
+-- vim.o.guifont = "JetBrains\\ Mono\\ Regular\\ Nerd\\ Font\\ Complete"
+vim.bo.smartindent = true -- Makes indenting smart
+vim.cmd('filetype plugin on') -- filetype detection
+vim.cmd('set colorcolumn=99999') -- fix indentline for now
+vim.cmd('set expandtab') -- Converts tabs to spaces
 vim.cmd('set sw=4') -- Change the number of space characters inserted for indentation
 vim.cmd('set ts=4') -- Insert 2 spaces for a tab
 vim.cmd('set whichwrap+=<,>,[,],h,l') -- move to next line with theses keys
@@ -18,11 +22,7 @@ vim.o.clipboard = "unnamedplus" -- Copy paste between vim and everything else
 vim.o.cmdheight = 2 -- More space for displaying messages
 vim.o.conceallevel = 0 -- So that I can see `` in markdown files
 vim.o.fileencoding = "utf-8" -- The encoding written to file
-
-vim.o.guifont = "JetBrains\\ Mono\\ Regular\\ Nerd\\ Font\\ Complete"
--- vim.o.guifont = "JetBrainsMono\\ Nerd\\ Font\\ Mono:h18"
-
-vim.o.hidden = true -- Required to keep multiple buffers open multiple buffers
+vim.o.guifont = "JetBrainsMono\\ Nerd\\ Font\\ Mono:h18"
 vim.o.mouse = "a" -- Enable your mouse
 vim.o.pumheight = 10 -- Makes popup menu smaller
 vim.o.showmode = false -- We don't need to see things like -- INSERT -- anymore
@@ -30,12 +30,7 @@ vim.o.showtabline = 2 -- Always show tabs
 vim.o.splitbelow = true -- Horizontal splits will automatically be below
 vim.o.splitright = true -- Vertical splits will automatically be to the right
 vim.o.termguicolors = true -- set term gui colors most terminals support this
-vim.o.timeoutlen = 1000 -- By default timeoutlen is 1000 ms
-vim.o.title = true
-vim.o.titlestring="%<%F%=%l/%L - nvim"
 vim.o.updatetime = 300 -- Faster completion
+vim.o.writebackup = false -- This is recommended by coc
 vim.wo.cursorline = true -- Enable highlighting of the current line
-vim.wo.number = false -- set numbered lines
-vim.wo.relativenumber = true -- set relative number
 vim.wo.signcolumn = "yes" -- Always show the signcolumn, otherwise it would shift the text each time
-vim.wo.wrap = false -- Display long lines as just one line
