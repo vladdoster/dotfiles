e8d8bc53c11b1857d9b5d4b3875f8cf1cc389438
Vladislav Doster <mvdoster@gmail.com>
2021-06-15 19:51:20 -0500

    (scripts) add install-guacamole.sh script
    Installs Oracle Guacamole. Guacamole is an HTML5 web application that
    provides access to desktop environments using remote desktop protocols
    (such as VNC or RDP).
    
    Signed-off-by: Vladislav Doster <mvdoster@gmail.com>

---
+++ bin/.local/bin/install-guacamole.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+echo "--- Installing guacamole dependencies"
+sudo yum install guacamole guacd
+echo "--- Installing guacamole packages for SSH, RDP, and VNC"
+yum install libguac-client-vnc libguac-client-ssh libguac-client-rdp
+echo "--- Setting user options"
+PS3="Enter a number: "
+select opt in "on boot" "create alias"
+do
+  case $character in 
+    "on boot")
+      echo "Setting to start on boot"
+      ln -s "/usr/lib/systemd/system/tomcat6.service" "/etc/systemd/system/multi-user.target.wants/tomcat6.service"
+      exit 0
+      ;;
+    *)
+      echo "--- Use 'service tomcat start' to  start guacamole"
+      exit 0
+      ;;
+  esac
+done
