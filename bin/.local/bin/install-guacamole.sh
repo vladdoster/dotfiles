b85104541f0b0f0a2fc591cb26d2e41d4d0aed20
Vladislav Doster <mvdoster@gmail.com>
2021-06-15 22:45:55 -0500

    (maint) add error handling to install-guacamole
    Signed-off-by: Vladislav Doster <mvdoster@gmail.com>

--- bin/.local/bin/install-guacamole.sh
+++ bin/.local/bin/install-guacamole.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+trap 'catch' ERR
+catch() {
+  echo "--- ERROR: An error has occurred while running $0"
+  exit 1
+}
+
 echo "--- Installing guacamole dependencies"
 sudo yum install guacamole guacd
 echo "--- Installing guacamole packages for SSH, RDP, and VNC"
@@ -7,15 +13,21 @@ yum install libguac-client-vnc libguac-client-ssh libguac-client-rdp
 echo "--- Setting user options"
 PS3="Enter a number: "
 select opt in "on boot" "create alias"; do
-    case $character in
-        "on boot")
+    case $REPLY in
+        1)
             echo "Setting to start on boot"
             ln -s "/usr/lib/systemd/system/tomcat6.service" "/etc/systemd/system/multi-user.target.wants/tomcat6.service"
-            exit 0
+            break
             ;;
-        *)
+        2)
+            echo "--- Use 'start-guacamole' to start guacamole"
+            break
+            ;;
+          *)
             echo "--- Use 'service tomcat start' to  start guacamole"
-            exit 0
+            break
             ;;
     esac
 done
+echo "--- Successfully installed guacamole."
+
