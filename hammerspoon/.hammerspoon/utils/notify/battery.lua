7b5db338433b89696d01f0c0d8ca4fd9028166c1
Vladislav Doster <mvdoster@gmail.com>
2021-12-23 22:56:17 -0600

    (maint): update hs battery notif logic
    - sets brightness to 100
    - remove redundant code
    - use subtitle for message instead of notification message
    
    Signed-off-by: Vladislav Doster <mvdoster@gmail.com>

--- hammerspoon/.hammerspoon/utils/notify/battery.lua
+++ hammerspoon/.hammerspoon/utils/notify/battery.lua
@@ -1,11 +1,12 @@
 -- module: Battery notifications
 local m = {}
 local battery = require('hs.battery')
-local isCharged, percentage, powerSource = battery.isCharged(), battery.percentage(), battery.powerSource()
+local isCharged, powerSource = battery.isCharged(), battery.powerSource()
+local notifThreshold = 5 -- 5% of battery
 -- send a notification about the battery status
 local function batteryNotify(statusType, subTitle, message)
     hs.notify.new({
-        title = statusType .. ' Status',
+        title = statusType .. ' Alert',
         subTitle = subTitle,
         soundName = 'Ping.aiff', -- /System/Library/Sounds/Ping.aiff
         informativeText = message,
@@ -13,25 +14,24 @@ local function batteryNotify(statusType, subTitle, message)
         autoWithdraw = true
     }):send()
 end
--- battery watching callback sends a notification if charging status
--- has changed, or power thresholds have been reached.
+-- battery watching callback sends a notification if charging status has changed or power thresholds are reached.
 local function watchFunc()
     local newPercentage = battery.percentage()
     local newIsCharged = battery.isCharged()
     local newPowerSource = battery.powerSource()
     if newPercentage < 100 then isCharged = false end
+    if newPowerSource ~= powerSource then
+        batteryNotify('Power Source Change', 'Source: ' .. newPowerSource)
+        powerSource = newPowerSource
+        hs.brightness.set(100)
+    end
+    if newPercentage <= notifThreshold and newPowerSource == "Battery Power" then
+        local timeRemaining = battery.timeRemaining()
+        batteryNotify('Low Battery', 'Time Remaining: ' .. timeRemaining .. ' minutes')
+    end
     if newIsCharged ~= isCharged and newPercentage == 100 and newPowerSource == 'AC Power' then
         batteryNotify('Battery', 'Completely Charged')
         isCharged = true
-    end
-    local threshold = 5 -- 5% of battery
-    if (newPercentage <= threshold and newPowerSource == "Battery Power") then
-        local timeRemaining = hs.battery.timeRemaining()
-        batteryNotify('Low Battery', 'Time Remaining:', 'is ' .. timeRemaining .. ' minutes')
-    end
-    if newPowerSource ~= powerSource and newPowerSource == "Battery Power" then
-        batteryNotify('Power Source Change', 'Current :', newPowerSource)
-        powerSource = newPowerSource
     end
 end
 function m.start() m.watcher = hs.battery.watcher.new(watchFunc):start() end
