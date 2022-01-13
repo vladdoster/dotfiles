068f8fe041a8604db65354b99d1bc088e2ad7c01
Vladislav Doster <vladislav.doster@mastercard.com>
2022-01-13 17:48:12 -0600

    (maint) pin zinit, zshenv, and kitty
    Signed-off-by: Vladislav Doster <vladislav.doster@mastercard.com>

--- zsh/.config/zsh/zinit.zsh
+++ zsh/.config/zsh/zinit.zsh
@@ -43,27 +43,30 @@ zturbo(){ zinit depth'1' lucid ${1/#[0-9][a-d]/wait"${1}"} "${@:2}"; }
 zi light-mode for \
   atinit"zicompinit; zicdreplay" \
     zdharma-continuum/fast-syntax-highlighting \
+  as'completion' \
+    OMZL::{'completion','key-bindings','termsupport'}.zsh \
+  is-snippet atinit' export VI_MODE_SET_CURSOR=true; export VI_MODE_RESET_PROMPT_ON_MODE_CHANGE=true' \
+    OMZP::vi-mode \
     "$ZI_REPO"/zinit-annex-{'submods','patch-dl','bin-gem-node','rust'} \
   compile'(pure|async).zsh' pick'async.zsh' src'pure.zsh' atload"
       PURE_GIT_UP_ARROW='↑'; PURE_GIT_DOWN_ARROW='↓'; PURE_PROMPT_SYMBOL='ᐳ'; PURE_PROMPT_VICMD_SYMBOL='ᐸ';
       zstyle ':prompt:pure:git:action' color 'yellow'; zstyle ':prompt:pure:git:branch' color 'blue'; zstyle ':prompt:pure:git:dirty' color 'red'
       zstyle ':prompt:pure:path' color 'cyan'; zstyle ':prompt:pure:prompt:success' color 'green'" \
-    sindresorhus/pure \
-  as'null' depth'1' nocompile nocompletions atpull'%atclone' atclone'./install -e no -d ~/.local' \
-    @romkatv/zsh-bin \
-    vladdoster/gitfast-zsh-plugin \
-  pack'bgn-binary+keys' id-as'package/fzf' fzf
+    sindresorhus/pure
+  #  as'null' depth'1' nocompile nocompletions atpull'%atclone' atclone'./install -e no -d ~/.local' \
+    #  @romkatv/zsh-bin \
+
 zturbo light-mode for \
-    OMZP::golang      as'completion' OMZP::golang/_golang                 \
-    OMZP::pip         as'completion' OMZP::pip/_pip                       \
-    OMZP::terraform   as'completion' OMZP::terraform/_terraform           \
+  vladdoster/gitfast-zsh-plugin \
+  pack'bgn-binary+keys' id-as'package/fzf' fzf
   has'brew'           as'completion' https://raw.githubusercontent.com/Homebrew/brew/master/completions/zsh/_brew \
   has'docker'         as'completion' OMZP::docker/_docker                 \
   has'docker-compose' as'completion' OMZP::docker-compose/_docker-compose \
+  has'go'        OMZP::golang      as'completion' OMZP::golang/_golang                 \
   has'npm'   OMZP::npm   \
+  has'pip'       OMZP::pip         as'completion' OMZP::pip/_pip                       \
   has'rsync' PZTM::rsync \
-  atload"VI_MODE_SET_CURSOR=true" \
-             OMZP::vi-mode \
+  has'terraform' OMZP::terraform   as'completion' OMZP::terraform/_terraform           \
   svn submods'zsh-users/zsh-completions -> external' \
   blockf atpull'zinit creinstall -q .' \
     PZT::modules/completion OMZL::completion.zsh \
@@ -71,14 +74,8 @@ zturbo light-mode for \
     PZT::modules/history-substring-search OMZL::history.zsh \
   svn submods'zsh-users/zsh-autosuggestions -> external' \
     PZT::modules/autosuggestions
-#=== RUST BINARIES ==========================================
-#  zturbo as'null' sbin'bin/*' rustup \
-  #  atload"[[ ! -f ${ZINIT[COMPLETIONS_DIR]}/_cargo ]] && zi creinstall rust \
-         #  && export CARGO_HOME=$PWD RUSTUP_HOME=$PWD/rustup" \
-  #  cargo'bat;fd-find;hyperfine;ripgrep;sd;skim;zenith;git-delta' for \
-    #  zdharma-continuum/null
 #=== GITHUB BINARIES ==========================================
-zturbo from'gh-r' bpick"${bpick}" as'program' for \
+zturbo from'gh-r' as'program' for \
   sbin'bat*/bat'     @sharkdp/bat     \
   sbin'delta*/delta' dandavison/delta \
   sbin'fd*/fd'       @sharkdp/fd      \
@@ -91,6 +88,12 @@ zturbo from'gh-r' bpick"${bpick}" as'program' for \
          alias ll='ls -al'; alias tree='exa --tree'
          alias ls='exa --git --group-directories-first'" \
     ogham/exa
+#=== RUST BINARIES ==========================================
+#  zturbo as'null' sbin'bin/*' rustup \
+  #  atload"[[ ! -f ${ZINIT[COMPLETIONS_DIR]}/_cargo ]] && zi creinstall rust \
+         #  && export CARGO_HOME=$PWD RUSTUP_HOME=$PWD/rustup" \
+  #  cargo'bat;fd-find;hyperfine;ripgrep;sd;skim;zenith;git-delta' for \
+    #  zdharma-continuum/null
 #  zturbo 1a as"program" bpick"*.tar.gz" nocompile'!' atpull'%atclone' for \
    #  make'-j bin/stow' pick"bin/stow" atclone"
       #  autoreconf -iv \
@@ -102,12 +105,12 @@ zturbo from'gh-r' bpick"${bpick}" as'program' for \
     #  @tmux/tmux
 
 # pip zsh completion start
-function _pip_completion {
-  local words cword
-  read -Ac words
-  read -cn cword
-  reply=( $( COMP_WORDS="$words[*]" \
-             COMP_CWORD=$(( cword-1 )) \
-             PIP_AUTO_COMPLETE=1 $words[1] 2>/dev/null ))
-}
-compctl -K _pip_completion pip3
+#  function _pip_completion {
+  #  local words cword
+  #  read -Ac words
+  #  read -cn cword
+  #  reply=( $( COMP_WORDS="$words[*]" \
+             #  COMP_CWORD=$(( cword-1 )) \
+             #  PIP_AUTO_COMPLETE=1 $words[1] 2>/dev/null ))
+#  }
+#  compctl -K _pip_completion pip3
--- zsh/.zshenv
+++ zsh/.zshenv
@@ -22,14 +22,14 @@ case $OSTYPE in
                 #  export CPPFLAGS="-I/usr/local/opt/expat/include:$CPPFLAGS"
                 #  export LDFLAGS="-L/usr/local/opt/curl/lib:$LDFLAGS"
                 #  export LDFLAGS="-L/usr/local/opt/expat/lib:$LDFLAGS"
+                #  export PKG_CONFIG_PATH="/usr/local/opt/curl/lib/pkgconfig:$PKG_CONFIG_PATH"
+                #  export PKG_CONFIG_PATH="/usr/local/opt/expat/lib/pkgconfig:$PKG_CONFIG_PATH"
                 export PATH="/usr/local/opt/curl/bin:$PATH"
                 export PATH="/usr/local/opt/expat/bin:$PATH"
                 export PATH="/usr/local/opt/libtool/libexec/gnubin:$PATH"
                 export PATH="/usr/local/opt/python@3.10/bin:$PATH"
                 export PATH="/usr/local/opt/sphinx-doc/bin:$PATH"
                 export PATH="/usr/local/opt/terraform@0.12/bin:$PATH"
-                #  export PKG_CONFIG_PATH="/usr/local/opt/curl/lib/pkgconfig:$PKG_CONFIG_PATH"
-                #  export PKG_CONFIG_PATH="/usr/local/opt/expat/lib/pkgconfig:$PKG_CONFIG_PATH"
             ;;
         esac
     ;;
@@ -43,32 +43,31 @@ case $OSTYPE in
     *) echo "--- ERROR: $OSTYPE is unsupported" && exit 1 ;;
 esac
 export KEYTIMEOUT=1
-#  ## -- Reserved Variables -------------------------------------------------------
-(( ${+XDG_CONFIG_HOME} )) || export XDG_CONFIG_HOME="$HOME/.config"
+# -- RESERVED VARIABLES -------------------------------------------------------
+(( ${+HOSTNAME} )) || export HOSTNAME="$HOST"
+(( ${+LANGUAGE} )) || export LANGUAGE="$LANG"
+(( ${+LANG}   )) || export LANG="en_US.UTF-8"
+(( ${+LC_ALL} )) || export LC_ALL="$LANG"
+(( ${+USER}   )) || export USER="$USERNAME"
 (( ${+XDG_CACHE_HOME}  )) || export XDG_CACHE_HOME="$HOME/.cache"
+(( ${+XDG_CONFIG_HOME} )) || export XDG_CONFIG_HOME="$HOME/.config"
 (( ${+XDG_DATA_HOME}   )) || export XDG_DATA_HOME="$HOME/.local/share"
-#  (( ${+USER}     )) || export USER="$USERNAME"
-#  (( ${+HOSTNAME} )) || export HOSTNAME="$HOST"
-#  (( ${+LANG}     )) || export LANG="en_US.UTF-8"
-#  (( ${+LANGUAGE} )) || export LANGUAGE="$LANG"
-#  (( ${+LC_ALL}   )) || export LC_ALL="$LANG"
-## Common Apps
+#-- COMMON APPS ---------------------------------------------------------------------
 #  export EDITOR="vim"
 export VISUAL=$EDITOR
 export FCEDIT=$EDITOR
 export SYSTEMD_EDITOR=$EDITOR
 export PAGER=less
 export MANPAGER="$PAGER"
-#  ## -- CDPATH ---------------------------------------------------------------------
-#  cdpath+=("$HOME" "..")
-#  export cdpath
-#  ## -- MANPATH ---------------------------------------------------------------------
-#  manpath+=(/usr/local/man /usr/share/man)
-#  export manpath
-## -- PATH ---------------------------------------------------------------------
-#  if [ -d "$HOME/.bin" ]; then export PATH="$PATH:$HOME/.bin" fi
-#  if [ -d "$HOME/.cargo/bin" ]; then export PATH="$PATH:$HOME/.cargo/bin" fi
+# -- CDPATH ---------------------------------------------------------------------
+cdpath+=("$HOME" "..") && export cdpath
+# -- MANPATH ---------------------------------------------------------------------
+manpath+=(/usr/local/man /usr/share/man) && export manpath
+# -- PATH ---------------------------------------------------------------------
 [ -d "$HOME/.local/bin" ] && export PATH="$PATH:$HOME/.local/bin"
+[ -d "$HOME/.local/bin" ] && export PATH="$PATH:$HOME/.local/bin" fi
+[ -d "/usr/local/bin" ] && export PATH="$PATH:/usr/local/bin"
+[ -d "/usr/sbin" ] && export PATH="$PATH:/usr/sbin"
 #  if [ -d "$HOME/.yarn/bin" ]; then export PATH="$PATH:$HOME/.yarn/bin" fi
 #  if [ -d "$HOME/Application" ]; then export PATH="$PATH:$HOME/Application" fi
 #  if [ -d "$HOME/Applications" ]; then export PATH="$PATH:$HOME/Applications" fi
@@ -77,6 +76,7 @@ export MANPAGER="$PAGER"
 #  if [ -d "/snap/bin" ]; then export PATH="$PATH:/snap/bin" fi
 #  if [ -d "/usr/local/bin" ]; then export PATH="$PATH:/usr/local/bin" fi
 #  if [ -d "/usr/sbin" ]; then export PATH="$PATH:/usr/sbin" fi
+#-- ENV --------------------------------------------------------------
 export AZURE_CONFIG_DIR="$XDG_DATA_HOME"/azure
 export DOTFILES="$XDG_CONFIG_HOME"/dotfiles
 export GIT_CONFIG="$XDG_CONFIG_HOME"/git/config
@@ -85,5 +85,6 @@ export PYTHONSTARTUP="$XDG_CONFIG_HOME"/python/init-repl.py
 export SUBVERSION_HOME="$XDG_CONFIG_HOME"/subversion
 export VIMDOTDIR="$XDG_CONFIG_HOME"/vim
 export ZDOTDIR="$XDG_CONFIG_HOME"/zsh
-#  typeset -agU cdpath fpath path
-#  path=( "${path[@]:#}" )
+#-- DE-DUPE PATH -------------------------------------------------------------
+typeset -agU cdpath fpath path
+path=( "${path[@]:#}" )
