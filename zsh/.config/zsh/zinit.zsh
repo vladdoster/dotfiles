5a32f3ef477fd85f0f4d4c7441c4b1ed9e5adb7c
Vladislav Doster <mvdoster@gmail.com>
2022-02-18 00:32:35 -0600

    bugfix: comment out binaries and reorder completions
    Signed-off-by: Vladislav Doster <mvdoster@gmail.com>

--- zsh/.config/zsh/zinit.zsh
+++ zsh/.config/zsh/zinit.zsh
@@ -45,57 +45,53 @@ zi light-mode for \
       zstyle ':prompt:pure:path' color 'cyan'; zstyle ':prompt:pure:prompt:success' color 'green'" \
     sindresorhus/pure
 #=== PLUGINS ==========================================
-zi lucid wait'!0' for \
-  atinit"zicompinit; zicdreplay" light-mode \
-    zdharma-continuum/fast-syntax-highlighting \
+zi lucid wait for \
+  atinit"zicompinit; zicdreplay" light-mode $ZI_REPO/fast-syntax-highlighting \
   atinit"VI_MODE_SET_CURSOR=true; bindkey -M vicmd '^e' edit-command-line" is-snippet \
-    OMZ::plugins/vi-mode \
+		OMZ::plugins/vi-mode \
   atinit"bindkey '^_' autosuggest-execute; bindkey '^ ' autosuggest-accept" \
-    zsh-users/zsh-autosuggestions \
-  atinit"HISTFILE=$HOME/.zhistory" \
-    PZT::modules/history
-zi lucid wait'1' for \
+		zsh-users/zsh-autosuggestions \
+  atinit"HISTFILE=$HOME/.zhistory" PZT::modules/history \
   as'completion' vladdoster/gitfast-zsh-plugin \
-  atpull'zinit creinstall -q .' blockf \
-  svn submods'zsh-users/zsh-completions -> external' \
+  atpull'zinit creinstall -q .' blockf svn submods'zsh-users/zsh-completions -> external' \
     PZT::modules/completion \
   svn submods'zsh-users/zsh-history-substring-search -> external' \
     OMZ::plugins/history-substring-search
 #=== COMPLETION ==========================================
-zi lucid wait'2' for \
-  is-snippet as'completion' https://raw.githubusercontent.com/Homebrew/brew/master/completions/zsh/_brew \
-  is-snippet as'completion' https://raw.githubusercontent.com/rust-lang/cargo/master/src/etc/_cargo \
-  is-snippet as'completion' https://raw.githubusercontent.com/docker/cli/master/contrib/completion/zsh/_docker \
-  has'go'  OMZP::golang as'completion' OMZP::golang/_golang \
-  has'npm' OMZP::npm \
-  has'pip' OMZP::pip as'completion' OMZP::pip/_pip \
-  has'rsync' PZTM::rsync \
-  has'terraform' OMZP::terraform as'completion' OMZP::terraform/_terraform
+zi lucid wait is-snippet as'completion' for \
+  OMZP::golang/_golang \
+  OMZP::pip/_pip \
+  OMZP::terraform/_terraform \
+  https://raw.githubusercontent.com/Homebrew/brew/master/completions/zsh/_brew \
+  https://raw.githubusercontent.com/docker/cli/master/contrib/completion/zsh/_docker \
+  https://raw.githubusercontent.com/rust-lang/cargo/master/src/etc/_cargo
+  #  has'npm' OMZP::npm \
+  #  has'rsync' PZTM::rsync \
 #=== GITHUB BINARIES ==========================================
 zi pip'black; isort; mdformat; mdformat-gfm; mdformat-tables; mdformat-toc; tldr; wheel' load for \
   "$ZI_REPO"/null
-zi from'gh-r' lucid nocompile for \
-  sbin'**/bat'  @sharkdp/bat          \
-  sbin'**/d*a'  dandavison/delta      \
-  sbin'**/fd'   @sharkdp/fd           \
-  sbin'**/g*r'  idc101/git-mkver      \
-  sbin'**/g*w'  charmbracelet/glow    \
-  sbin'**/gh'   cli/cli               \
-  sbin'**/h*e'  @sharkdp/hyperfine    \
-  sbin'**/l*t'  jesseduffield/lazygit \
-  sbin'**/nvim' atinit'alias v=nvim' ver'nightly' neovim/neovim \
-  sbin'**/p*s'  dalance/procs \
-  sbin'**/rg'   BurntSushi/ripgrep \
-  sbin'**/t*i' XAMPPRocky/tokei  \
-  sbin'fzf'    junegunn/fzf      \
-  sbin'g*r'    @github/git-sizer \
-  sbin'g*x'    pemistahl/grex    \
-  sbin'**/sh* -> shfmt' @mvdan/sh             \
-  sbin'**/exa'  atclone'cp -vf completions/exa.zsh _exa' atinit"
-      alias l='exa -blF'; alias la='exa -abghilmu'
-      alias ll='exa -al'; alias tree='exa --tree'
-      alias ls='exa --git --group-directories-first'" \
-    ogham/exa
+#  zi from'gh-r' lucid nocompile for \
+  #  sbin'**/bat'  @sharkdp/bat          \
+  #  sbin'**/d*a'  dandavison/delta      \
+  #  sbin'**/fd'   @sharkdp/fd           \
+  #  sbin'**/g*r'  idc101/git-mkver      \
+  #  sbin'**/g*w'  charmbracelet/glow    \
+  #  sbin'**/gh'   cli/cli               \
+  #  sbin'**/h*e'  @sharkdp/hyperfine    \
+  #  sbin'**/l*t'  jesseduffield/lazygit \
+  #  sbin'**/nvim' atinit'alias v=nvim' ver'nightly' neovim/neovim \
+  #  sbin'**/p*s'  dalance/procs \
+  #  sbin'**/rg'   BurntSushi/ripgrep \
+  #  sbin'**/t*i' XAMPPRocky/tokei  \
+  #  sbin'fzf'    junegunn/fzf      \
+  #  sbin'g*r'    @github/git-sizer \
+  #  sbin'g*x'    pemistahl/grex    \
+  #  sbin'**/sh* -> shfmt' @mvdan/sh             \
+  #  sbin'**/exa'  atclone'cp -vf completions/exa.zsh _exa' atinit"
+      #  alias l='exa -blF'; alias la='exa -abghilmu'
+      #  alias ll='exa -al'; alias tree='exa --tree'
+      #  alias ls='exa --git --group-directories-first'" \
+    #  ogham/exa
 #=== PIP COMPLETION
 function _pip_completion {
   local words cword && read -Ac words && read -cn cword
