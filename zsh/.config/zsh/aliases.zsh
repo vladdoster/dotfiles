15ff0fc4b9c677e99290a7a1c5e5864ca3f776ed
Vladislav Doster <mvdoster@gmail.com>
2022-05-30 20:22:44 -0500

    style: change section headers to boxes
    Signed-off-by: Vladislav Doster <mvdoster@gmail.com>

--- zsh/.config/zsh/aliases.zsh
+++ zsh/.config/zsh/aliases.zsh
@@ -19,16 +19,22 @@ _goto(){ [[ -e "$1" ]] && cd "$1" && $(which exa) --all --long || ls -lGo; }
 _mkfile() { echo "#!/usr/bin/env ${2}" >"${3}.${1}" && chmod +x "${3}.${1}"; }
 _sys_update() { "$1" update && "$1" upgrade; }
 has() { command -v "$1" 1>/dev/null 2>&1; }
-#= CODE DIRECTORY ===============================
+# +────────────────+
+# │ CODE DIRECTORY │
+# +────────────────+
 CODE_DIR="${HOME:-~}/code"
 ! [[ -d "$CODE_DIR" ]] && mkdir -p "${CODE_DIR}"
-#= SYSTEM SPECIFIC ===============================
+# +─────────────────+
+# │ SYSTEM SPECIFIC │
+# +─────────────────+
 if [[ $OSTYPE =~ darwin* ]]; then
 	_copy_cmd='pbcopy -pboard general'
 	alias readlink="greadlink"
 	alias copy="$_copy_cmd <"
 fi
-#= FILE LOCATIONS ================================
+# +───────+
+# │ FILES │
+# +───────+
 alias b="cd -"
 alias cpv="rsync -ah --info=progress2"
 alias d='dirs -v --'
@@ -47,7 +53,9 @@ fi
 if has python3; then
 	alias python='python3'
 fi
-#= CONFIG SHORTCUTS ==============================
+# +──────────────────+
+# │ CONFIG SHORTCUTS │
+# +──────────────────+
 cfg_alias(){ alias ${1}="_edit ${XDG_CONFIG_HOME}/${2}"; }
 cfg_alias 'ealiases' 'zsh/aliases.zsh'
 cfg_alias 'gignore'  'git/ignore'
@@ -58,17 +66,23 @@ cfg_alias 'skhdrc'   'skhd/skhdrc'
 cfg_alias 'tmuxrc'   'tmux/tmux.conf'
 cfg_alias 'zic'      'zsh/zinit.zsh'
 cfg_alias 'zsc'      'zsh/.zshrc'
-#= HOME SHORTCUTS ================================
+# +────────────────+
+# │ HOME SHORTCUTS │
+# +────────────────+
 home_alias(){ alias ${1}="_edit ${HOME}/${2}"; }
 home_alias 'hscfg' '.hammerspoon/init.lua'
 home_alias 'sshrc' '.ssh/config'
 home_alias 'zec'   '.zshenv'
-#= RELOAD COMMANDS ===============================
+# +─────────────────+
+# │ RELOAD COMMANDS │
+# +─────────────────+
 alias nvcln="rm -frv $HOME/.{local/share/nvim,config/nvim/plugin}"
 alias zcln="rm -fr ${HOME}/.{local/share/{zinit,zsh},cache,config/{zinit,zsh/.{zcomp{cache,dump},zsh_sessions}}}"
 alias zreset="pushd ${HOME} && zcln && zrld"
 alias zrld="exec zsh"
-#= NAVIGATION ====================================
+# +────────────+
+# │ NAVIGATION │
+# +────────────+
 cd_alias(){ alias ${1}="_goto ${2}"; }
 cd_alias '..' '..'
 cd_alias '...' '../..'
@@ -87,22 +101,30 @@ cd_alias 'zd' '$ZDOTDIR'
 cd_alias 'zid' '$ZINIT[HOME_DIR]'
 cd_alias 'zigd' '$ZINIT[BIN_DIR]'
 cd_alias 'zinstall' '$ZINIT[BIN_DIR]/zinit-install.zsh'
-#= GIT ===========================================
+# +─────+
+# │ GIT │
+# +─────+
 alias g-submodule-update='git submodule update --merge --remote'
 alias g="git" # GIT ALIASES DEFINED IN $HOME/.config/git/config
-#= COMMAND SHORTCUTS =============================
+# +───────────────────+
+# │ COMMAND SHORTCUTS │
+# +───────────────────+
 alias rm-ds-store='find "$PWD" -type f -name "*.DS[_-]Store" -print -delete'
 alias rshfmt="shfmt -i 4 -s -ln bash -sr -bn -ci -w"
 alias zc='zinit compile'
 alias zp='zinit times'
 alias zt='hyperfine --warmup 100 --runs 10000 "/bin/ls"'
 alias zclnplg="$(rm -rf $ZINIT[PLUGINS_DIR])"
-#= MISC. =========================================
+# +───────+
+# │ MISC. │
+# +───────+
 alias gen-passwd='openssl rand -base64 24'
 alias get-my-ip='curl ifconfig.co'
 alias tmp-md='$EDITOR $(mktemp -t scratch.XXX.md)'
 alias ps-grep="ps aux | grep -v grep | grep -i -e VSZ -e"
-#= PYTHON ========================================
+# +────────+
+# │ PYTHON │
+# +────────+
 alias http-serve='python3 -m http.server'
 alias p='python3'
 alias pip-requirements='pip3 install -r requirements.txt || _error "no requirements.txt found"'
@@ -110,24 +132,34 @@ alias ppip='python3 -m pip'
 alias venv-activate='source ./.venv/bin/activate'
 alias venv-create='python3 -m venv ./.venv'
 alias venv-setup='venv-create && venv-activate && pip-requirements'
-#= NETWORK INFO ==================================
+# +──────────────+
+# │ NETWORK INFO │
+# +──────────────+
 alias get-open-ports='sudo lsof -i -n -P | grep TCP'
 alias ping='ping -c 10'
-#= FILE CREATION =================================
+# +───────────────+
+# │ FILE CREATION │
+# +───────────────+
 alias mkmd='{ F_NAME="$(cat -).md"; touch "$F_NAME"; _info "created: $F_NAME"; }<<<'
 alias mkpy='_mkfile py "python3"'
 alias mksh='_mkfile sh "bash"'
 alias mktxt='{ F_NAME="$(cat -).txt"; touch "$F_NAME"; _info "created: $F_NAME"; }<<<'
 alias mkzsh='_mkfile zsh "zsh"'
 mkcd(){ mkdir -p -- "$1" && cd -P -- "$1" ;}
-#= FILE FORMATTING ===============================
-# alias fmtlua='_fmt lua "stylua -i"'
-# alias fmtmd="_fmt md mdformat"
-# alias fmtpy="_fmt py python3 -m black"
-# alias fmtsh='_fmt sh "shfmt -bn -ci -i 4 -ln=bash -s -sr -w"'
-#= SYS ===========================================
+# +─────────────────+
+# │ FILE FORMATTING │
+# +─────────────────+
+alias fmtlua='_fmt lua "stylua -i"'
+alias fmtmd="_fmt md mdformat"
+alias fmtpy="_fmt py python3 -m black"
+alias fmtsh='_fmt sh "shfmt -bn -ci -i 4 -ln=bash -s -sr -w"'
+# +─────+
+# │ SYS │
+# +─────+
 alias what-system='echo OSTYPE=${OSTYPE} MACHTYPE=${MACHTYPE} CPUTYPE=${CPUTYPE} hardware=$(uname -m) processor=$(uname -p)'
-#= REMOTE =========================================
+# +────────+
+# │ REMOTE │
+# +────────+
 alias apt-sys-update="_sys_update 'sudo apt --yes'"
 alias brew-clean="brew cleanup --prune=all"
 alias brew-reset="brew update-reset"
