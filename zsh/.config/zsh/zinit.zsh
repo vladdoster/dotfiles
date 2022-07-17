63244648e8d329fd9b4300b0110d2e3af806ddda
Vladislav Doster <mvdoster@gmail.com>
2022-07-16 20:25:01 -0500

    feat: use kitty split layout && format zinit config
    Signed-off-by: Vladislav Doster <mvdoster@gmail.com>

--- zsh/.config/zsh/zinit.zsh
+++ zsh/.config/zsh/zinit.zsh
@@ -5,15 +5,9 @@
 # macOS and Linux.
 #
 #=== HELPER METHODS ===================================
-error() {
-  # print -P "%F{red}[ERROR]%f: %F{yellow}$1%f" && return 1
-  print -P "[ERROR]: $1"
-  return 1
-}
-info() {
-  # print -P "%F{blue}[INFO]%f: %F{cyan}$1%f"
-  print -P "[INFO]: $1"
-}
+error() { print -P "%F{red}[ERROR]%f: %F{yellow}$1%f" && return 1; }
+info() { print -P "%F{blue}[INFO]%f: %F{cyan}$1%f"; }
+# print -P "[ERROR]: $1" print -P "[INFO]: $1"
 #=== ZINIT ============================================
 typeset -gAH ZINIT;
 ZINIT[HOME_DIR]=$XDG_DATA_HOME/zsh/zinit  ZPFX=$ZINIT[HOME_DIR]/polaris
@@ -21,34 +15,43 @@ ZINIT[BIN_DIR]=$ZINIT[HOME_DIR]/zinit.git ZINIT[OPTIMIZE_OUT_DISK_ACCESSES]=1
 ZINIT[COMPLETIONS_DIR]=$ZINIT[HOME_DIR]/completions ZINIT[SNIPPETS_DIR]=$ZINIT[HOME_DIR]/snippets
 ZINIT[ZCOMPDUMP_PATH]=$ZINIT[HOME_DIR]/zcompdump    ZINIT[PLUGINS_DIR]=$ZINIT[HOME_DIR]/plugins
 ZI_FORK='vladdoster'; ZI_REPO='zdharma-continuum'; GH_RAW_URL='https://raw.githubusercontent.com'
-if [[ ! -e $ZINIT[BIN_DIR] ]]; then
-  info 'downloading zinit' \
-    && command git clone \
-    https://github.com/$ZI_REPO/zinit.git \
-    $ZINIT[BIN_DIR] \
-    || error 'failed to clone zinit repository' \
-    && info 'setting up zinit' \
-    && command chmod g-rwX $ZINIT[HOME_DIR] \
-    && zcompile $ZINIT[BIN_DIR]/zinit.zsh \
-    && info 'sucessfully installed zinit'
-fi
+if [[ ! -e $ZINIT[BIN_DIR] ]] {
+    info 'downloading zinit'
+    command git clone https://github.com/$ZI_REPO/zinit.git $ZINIT[BIN_DIR] \
+        || error 'failed to clone zinit repository'
+    command chmod g-rwX $ZINIT[HOME_DIR] \
+        && zcompile $ZINIT[BIN_DIR]/zinit.zsh \
+        && info 'sucessfully installed zinit'
+}
 if [[ -e $ZINIT[BIN_DIR]/zinit.zsh ]]; then
-  source $ZINIT[BIN_DIR]/zinit.zsh \
-    && autoload -Uz _zinit \
-    && (( ${+_comps} )) \
-    && _comps[zinit]=_zinit
-else error "unable to find 'zinit.zsh'" && return 1
+    source $ZINIT[BIN_DIR]/zinit.zsh \
+        && autoload -Uz _zinit \
+        && (( ${+_comps} )) \
+        && _comps[zinit]=_zinit
+else
+    error "unable to find 'zinit.zsh'" && return 1
 fi
 #=== STATIC ZSH BINARY =======================================
-# zi for atpull"%atclone" depth"1" lucid nocompile nocompletions as"null" \
-#   atclone"./install -e no -d ~/.local" atinit"export PATH=$HOME/.local/bin:$PATH" \
-#   @romkatv/zsh-bin
+zi \
+    as"null" \
+    atclone"./install -e no -d ~/.local" \
+    atinit"export PATH=$HOME/.local/bin:$PATH" \
+    atpull"%atclone" \
+    depth"1" \
+    lucid \
+    nocompile \
+    nocompletions \
+    for @romkatv/zsh-bin
 # #=== OH-MY-ZSH & PREZTO PLUGINS =======================
-zi for is-snippet \
-  OMZL::{'clipboard','compfix','completion','git','grep','key-bindings'}.zsh \
-  OMZP::{'brew','npm'} \
-  PZT::modules/{'history','rsync'}
-zi as'completion' for OMZP::{'golang/_golang','pip/_pip','terraform/_terraform'}
+zi \
+    is-snippet \
+    for \
+    OMZL::{'clipboard','compfix','completion','git','grep','key-bindings'}.zsh \
+    OMZP::{'brew','npm'} \
+    PZT::modules/{'history','rsync'}
+zi \
+    as'completion' \
+    for OMZP::{'golang/_golang','pip/_pip','terraform/_terraform'}
 #=== COMPLETIONS ======================================
 local GH_RAW_URL='https://raw.githubusercontent.com'
 install_completion() { zinit for as'completion' nocompile id-as"$1" is-snippet "$GH_RAW_URL/$2"; }
@@ -58,7 +61,7 @@ install_completion 'exa-completion/_exa'       'ogham/exa/master/completions/zsh
 install_completion 'fd-completion/_fd'         'sharkdp/fd/master/contrib/completion/_fd'
 #=== PROMPT ===========================================
 zi light-mode for \
-  compile'(pure|async).zsh' multisrc'(pure|async).zsh' atinit"
+    compile'(pure|async).zsh' multisrc'(pure|async).zsh' atinit"
 PURE_GIT_DOWN_ARROW='↓'; PURE_GIT_UP_ARROW='↑'
 PURE_PROMPT_SYMBOL='ᐳ'; PURE_PROMPT_VICMD_SYMBOL='ᐸ'
 zstyle ':prompt:pure:git:action' color 'yellow'
@@ -66,7 +69,7 @@ zstyle ':prompt:pure:git:branch' color 'blue'
 zstyle ':prompt:pure:git:dirty' color 'red'
 zstyle ':prompt:pure:path' color 'cyan'
 zstyle ':prompt:pure:prompt:success' color 'green'" \
-  sindresorhus/pure
+    sindresorhus/pure
 #=== zsh-vim-mode cursor configuration [[[
 MODE_CURSOR_VICMD="green block";              MODE_CURSOR_VIINS="#20d08a blinking bar"
 MODE_INDICATOR_REPLACE='%F{9}%F{1}REPLACE%f'; MODE_INDICATOR_VISUAL='%F{12}%F{4}VISUAL%f'
@@ -76,111 +79,144 @@ export KEYTIMEOUT=1; export LESS='-RMs'; export PAGER=less; export VISUAL=vi;
 # LC_CTYPE=en_US.UTF-8; LC_ALL=en_US.UTF-8
 #=== ANNEXES ==========================================
 zi light-mode for "$ZI_REPO"/zinit-annex-{'binary-symlink','patch-dl','submods','readurl'}
-zi light-mode for ver'style/format-md-and-zsh' "$ZI_REPO"/zinit-annex-bin-gem-node
 #=== GITHUB BINARIES ==================================
 zi from'gh-r' lbin'!' nocompile for \
-  @{'dandavison/delta','junegunn/fzf','koalaman/shellcheck','pemistahl/grex'} \
-  @{'r-darwish/topgrade','sharkdp/fd','sharkdp/hyperfine','itchyny/gojq'} \
-  blockf atclone'**/bin/gh completion --shell zsh > _gh' lbin'!**/bin/gh' @cli/cli \
-  bpick'*extended*' @gohugoio/hugo \
-  lbin'!* -> checkmake' @mrtazz/checkmake \
-  lbin'!* -> jq'     @stedolan/jq \
-  lbin'!* -> shfmt'  @mvdan/sh \
-  lbin'!* -> stylua' @JohnnyMorganz/StyLua  \
-  lbin'!**/rg'       @BurntSushi/ripgrep \
-  lbin'!**/bin/nvim' ver'nightly' @neovim/neovim \
-  lbin'!**/exa' atinit"alias l='exa -blF';
-    alias la='exa -abghilmu'; alias ll='exa -al'
-    alias ls='exa --git --group-directories-first'" \
-  @ogham/exa
+    @{'dandavison/delta','junegunn/fzf','koalaman/shellcheck','pemistahl/grex'} \
+    @{'r-darwish/topgrade','sharkdp/fd','sharkdp/hyperfine','itchyny/gojq'} \
+    blockf atclone'**/bin/gh completion --shell zsh > _gh' lbin'!**/bin/gh' @cli/cli \
+    bpick'*extended*' @gohugoio/hugo \
+    lbin'!* -> checkmake' @mrtazz/checkmake \
+    lbin'!* -> jq'     @stedolan/jq \
+    lbin'!* -> shfmt'  @mvdan/sh \
+    lbin'!* -> stylua' @JohnnyMorganz/StyLua  \
+    lbin'!**/rg'       @BurntSushi/ripgrep \
+    lbin'!**/bin/nvim' ver'nightly' @neovim/neovim \
+    lbin'!**/exa' atinit"alias l='exa -blF';
+alias la='exa -abghilmu'; alias ll='exa -al'
+alias ls='exa --git --group-directories-first'" \
+    @ogham/exa
 #=== UNIT TESTING =====================================
-# zi as'command' for \
-#     pick'src/semver' \
-#   vladdoster/semver-tool \
-#     pick'revolver' \
-#   @molovo/revolver \
-#     atclone'./build.zsh' pick'zunit' \
-#   @zdharma-continuum/zunit
+zi as'command' for \
+    pick'src/semver' \
+    vladdoster/semver-tool \
+    pick'revolver' \
+    @molovo/revolver \
+    atclone'./build.zsh' pick'zunit' \
+    @zdharma-continuum/zunit
+zi as'command' for \
+    pick'src/semver' vladdoster/semver-tool \
+    pick'revolver' @molovo/revolver \
+    atclone'./build.zsh' pick'zunit' @zdharma-continuum/zunit
 #=== COMPILED PROGRAMS ================================
-# zi for \
-#  as'null' \
-#  atclone'autoconf && ./configure --disable-utf8proc --prefix=$PWD' \
-#  atpull'%atclone' \
-#  extract'!' \
-#  from'gh-r' \
-#  id-as'tmux' \
-#  lbin'!' \
-#  lucid \
-#  make'-j 8 install' \
-#  nocompile \
-#  ver'latest' \
-#  wait'1' \
-#@tmux/tmux
+zi \
+    lucid \
+    make'PREFIX=$PWD install' \
+    nocompile \
+    for \
+    lbin'!**/bin/tree'  Old-Man-Programmer/tree \
+    lbin'!**/zsd(-*|)$' $ZI_REPO/zshelldoc
 
-zi lucid make'PREFIX=$PWD install' nocompile for \
-  lbin'!**/bin/tree' Old-Man-Programmer/tree \
-  lbin'!**/zsd(-*|)$' $ZI_REPO/zshelldoc
+zi \
+    as'null' \
+    atclone'./configure --disable-utf8proc --prefix=$PWD' \
+    atpull'%atclone' \
+    extract'!' \
+    from'gh-r' \
+    id-as'tmux' \
+    make'PREFIX=$PWD install'\
+    nocompile \
+    sbin \
+    ver'latest' \
+    for @tmux/tmux
 
-# zi for \
-  #     as'completion' atpull'%atclone' depth'1' atclone"./configure --prefix=$PWD" \
-  #     make"PREFIX=$ZPFX install" nocompile lbin"!build/luarocks" \
-  #   luarocks/luarocks
-# zinit for as'completions' atclone'./buildx* completion zsh > _buildx' from'gh-r' nocompile lbin'!buildx-* -> buildx' @docker/buildx
+zi \
+    as'null' \
+    atclone'autoreconf -iv && ./configure --prefix=$PWD' \
+    atpull'%atclone' \
+    lbin'!**/stow' \
+    make'PREFIX=$PWD install' \
+    nocompile \
+    for @aspiers/stow
+
+zi \
+    as'completion' \
+    atclone"./configure --prefix=$PWD" \
+    atpull'%atclone' \
+    depth'1' \
+    lbin"!build/luarocks" \
+    make"PREFIX=$ZPFX install" \
+    nocompile \
+    for @luarocks/luarocks
+
+zi \
+    as'completions' \
+    atclone'./buildx* completion zsh > _buildx' \
+    from'gh-r' \
+    lbin'!buildx-* -> buildx' \
+    nocompile \
+    for @docker/buildx
+
+zi \
+    as'completion' \
+    atclone'luarocks completion zsh > _luarocks' \
+    lucid \
+    nocompile \
+    for $ZI_REPO/null
 #=== PYTHON ===========================================
-# _pip_completion() {
-#   local words cword; read -Ac words; read -cn cword
-#   reply=($(
-#       COMP_WORDS="$words[*]"
-#       COMP_CWORD=$(( cword-1 )) PIP_AUTO_COMPLETE=1 $words 2>/dev/null
-# ))}; compctl -K _pip_completion pip3
+_pip_completion() {
+    local words cword; read -Ac words; read -cn cword
+    reply=($(
+            COMP_WORDS="$words[*]"
+            COMP_CWORD=$(( cword-1 )) PIP_AUTO_COMPLETE=1 $words 2>/dev/null
+))}; compctl -K _pip_completion pip3
 #=== MISC. ============================================
-# has'tmux' \
-#   thewtex/tmux-mem-cpu-load \
 zi light-mode lucid wait'1' for \
     atinit'bindkey -M vicmd "^v" edit-command-line' \
     compile'zsh-vim-mode*.zsh' \
-  softmoth/zsh-vim-mode \
+    softmoth/zsh-vim-mode \
+    thewtex/tmux-mem-cpu-load \
     submods'zsh-users/zsh-history-substring-search -> external' \
     svn \
-  OMZ::plugins/history-substring-search \
+    OMZ::plugins/history-substring-search \
     atpull'zinit creinstall -q .' \
     blockf \
-  zsh-users/zsh-completions \
+    zsh-users/zsh-completions \
     atload'_zsh_autosuggest_start' \
     atinit'\
     ZSH_AUTOSUGGEST_BUFFER_MAX_SIZE=50;\
     bindkey "^_" autosuggest-execute;\
     bindkey "^ " autosuggest-accept' \
-  zsh-users/zsh-autosuggestions \
+    zsh-users/zsh-autosuggestions \
     atinit'bindkey "^R" history-search-multi-word' \
-  $ZI_REPO/history-search-multi-word \
+    $ZI_REPO/history-search-multi-word \
     atclone'(){local f;cd -q →*;for f (*~*.zwc){zcompile -Uz -- ${f}};}' \
     atload'FAST_HIGHLIGHT[chroma-man]=' atpull'%atclone' \
     compile'.*fast*~*.zwc' nocompletions \
-  $ZI_REPO/fast-syntax-highlighting
+    $ZI_REPO/fast-syntax-highlighting
 
-zi for \
-    as'null' id-as'zinit/cleanup' lucid nocd wait'!' \
-    atload'
-      zicompinit; zicdreplay;
-      _zsh_highlight_bind_widgets
-      _zsh_autosuggest_bind_widgets' \
-  /dev/null
+zi \
+    as'null' \
+    id-as'zinit/cleanup' \
+    lucid \
+    nocd \
+    wait'!' \
+    atload'\
+    _zsh_autosuggest_bind_widgets; _zsh_highlight_bind_widgets; \
+    zicompinit; zicdreplay' \
+    for @$ZI_REPO/null
 
-#=== COMPILE ZSH SOURCE =======================================
-# zi for atpull'%atclone' nocompile as'null' atclone'
-#     { print -P "%F{blue}[INFO]%f:%F{cyan}Building Zsh %f" \
-#           && autoreconf --force --install --make || ./Util/preconfig \
-#           && CFLAGS="-g -O3" ./configure --prefix=/usr/local >/dev/null \
-#           && print -P "%F{blue}[INFO]%f:%F{cyan} Configured Zsh %f" \
-#           && make -j8 PREFIX=/usr/local >/dev/null || make \
-#           && print -P "%F{blue}[INFO]%f:%F{green} Compiled $(/usr/local/bin/zsh --version) %f" \
-#           && sudo make -j8 install >/dev/null || make \
-#           && print -P "%F{blue}[INFO]%f:%F{green} Installed $(/usr/local/bin/zsh --version) @ /usr/local/bin/zsh %f" \
-#           && print -P "%F{blue}[INFO]%f:%F{green} Adding /usr/local/bin/zsh to /etc/shells %f" \
-#           sudo sh -c "echo /usr/bin/local/zsh >> /etc/shells" \
-#           && print -P "%F{blue}[INFO]%f: To update your shell, run: %F{cyan} chsh --shell /usr/local/bin/zsh $USER %f"
-#     } || { print -P "%F{red}[ERROR]%f:%F{yellow} Failed to install Zsh %f" }' \
-#   zsh-users/zsh
+# { print -P "%F{blue}[INFO]%f:%F{cyan}Building Zsh %f" \
+    #     && autoreconf --force --install --make || ./Util/preconfig \
+    #     && CFLAGS="-g -O3" ./configure --prefix=/usr/local >/dev/null \
+    #     && print -P "%F{blue}[INFO]%f:%F{cyan} Configured Zsh %f" \
+    #     && make -j8 PREFIX=/usr/local >/dev/null || make \
+    #     && print -P "%F{blue}[INFO]%f:%F{green} Compiled $(/usr/local/bin/zsh --version) %f" \
+    #     && sudo make -j8 install >/dev/null || make \
+    #     && print -P "%F{blue}[INFO]%f:%F{green} Installed $(/usr/local/bin/zsh --version) @ /usr/local/bin/zsh %f" \
+    #     && print -P "%F{blue}[INFO]%f:%F{green} Adding /usr/local/bin/zsh to /etc/shells %f" \
+    #     sudo sh -c "echo /usr/bin/local/zsh >> /etc/shells" \
+    #     && print -P "%F{blue}[INFO]%f: To update your shell, run: %F{cyan} chsh --shell /usr/local/bin/zsh $USER %f"
+# } || { print -P "%F{red}[ERROR]%f:%F{yellow} Failed to install Zsh %f" }' \
+    #   zsh-users/zsh
 
-# vim:ft=zsh:sw=2:sts=2
+# vim:ft=zsh:sw=2:sts=2:et
--- zsh/.zshenv
+++ zsh/.zshenv
@@ -6,47 +6,51 @@
 # Open an issue in https://github.com/vladdoster/dotfiles if
 # you find a bug, have a feature request, or a question.
 #
-# SYSTEM SPECIFIC
-
+# +─────────────────+
+# │ SYSTEM SPECIFIC │
+# +─────────────────+
 # $- includes i if bash is interactive, allowing a shell script or startup file to test this state
 _def () { [[ ! -z "${(tP)1}" ]]; }
 _echo() { [[ $- == *i* ]] && print -P "${1}"; }
 path_append() {
-  for ARG in "$@"; do
-    if [ -d "$ARG" ] && [[ ":$PATH:" != *":$ARG:"* ]]; then
-      PATH="${PATH:+"$PATH:"}$ARG"
-      _echo "%F{blue}[INFO]%f: %F{cyan}Appended to PATH%f -> %F{green}${ARG}%f"
-    fi
-  done
+    for ARG in "$@"; do
+        if [ -d "$ARG" ] && [[ ":$PATH:" != *":$ARG:"* ]]; then
+            PATH="${PATH:+"$PATH:"}$ARG"
+            _echo "%F{blue}[INFO]%f: %F{cyan}Appended to PATH%f -> %F{green}${ARG}%f"
+        fi
+    done
 }
 activate_brew() {
-  LOCATIONS=( '$HOME/.linuxbrew/Homebrew' '/home/linuxbrew/.linuxbrew' '/opt/homebrew' '/usr/local' )
-  for F_PATH in $LOCATIONS; do
-    if [[ -e "${F_PATH}"/bin/brew  ]] {
-      _echo "%F{blue}[INFO]%f: %F{cyan}OS%f @ %F{green}${OSTYPE} [$(uname -m)]%f"
-      if eval "$( ${F_PATH}/bin/brew shellenv )"; then
-        _echo "%F{blue}[INFO]%f: %F{cyan}Homebrew%f @ %F{green}${F_PATH}/bin/brew%f"
-        break
-      fi
-    }
-  done
+    LOCATIONS=( '$HOME/.linuxbrew/Homebrew' '/home/linuxbrew/.linuxbrew' '/opt/homebrew' '/usr/local' )
+    for F_PATH in $LOCATIONS; do
+        if [[ -e "${F_PATH}"/bin/brew  ]] {
+            _echo "%F{blue}[INFO]%f: %F{cyan}OS%f @ %F{green}${OSTYPE} [$(uname -m)]%f"
+            if eval "$( ${F_PATH}/bin/brew shellenv )"; then
+                _echo "%F{blue}[INFO]%f: %F{cyan}Homebrew%f @ %F{green}${F_PATH}/bin/brew%f"
+                break
+            fi
+        }
+    done
 }
 activate_brew
-# RESERVED VARIABLES
+# +────────────────────+
+# │ RESERVED VARIABLES │
+# +────────────────────+
 local usr_path="/usr/local/opt" brew_path="/opt/homebrew/opt"
 path_append \
-  "${HOME}/.cargo/bin" \
-  "${HOME}/.local/bin" \
-  "${HOME}/Library/Python/3.8/bin" \
-  "${HOME}/Library/Python/3.9/bin" \
-  "${brew_path}/llvm/bin" \
-  "${brew_path}/make/libexec/gnubin" \
-  "${usr_path}/binutils/bin" \
-  "${usr_path}/coreutils/libexec/gnubin" \
-  "${usr_path}/gnu-sed/libexec/gnubin" \
-  "${usr_path}/gnu-tar/libexec/gnubin"
-# ENV VARIABLES
-(( ${+HOSTNAME} )) || export HOSTNAME="$HOST"
+    "${HOME}/.cargo/bin" \
+    "${HOME}/.local/bin" \
+    "${HOME}/Library/Python/3.8/bin" \
+    "${HOME}/Library/Python/3.9/bin" \
+    "${brew_path}/llvm/bin" \
+    "${brew_path}/make/libexec/gnubin" \
+    "${usr_path}/binutils/bin" \
+    "${usr_path}/coreutils/libexec/gnubin" \
+    "${usr_path}/gnu-sed/libexec/gnubin" \
+    "${usr_path}/gnu-tar/libexec/gnubin"
+# +─────────────────+
+# │ # ENV VARIABLES │
+# +─────────────────+
 (( ${+LANGUAGE} )) || export LANGUAGE="$LANG"
 (( ${+USER} )) || export USER="$USERNAME"
 (( ${+XDG_CACHE_HOME} )) || export XDG_CACHE_HOME="$HOME/.cache"
@@ -60,13 +64,14 @@ export PYTHONSTARTUP="$XDG_CONFIG_HOME"/python/init-repl.py
 export SUBVERSION_HOME="$XDG_CONFIG_HOME"/subversion
 export VIMDOTDIR="$XDG_CONFIG_HOME"/vim
 export ZDOTDIR="$XDG_CONFIG_HOME"/zsh
-# ENV VARIABLES
+
+export ARCHPREFERENCE=arm64,x86_64
 export COMPOSE_DOCKER_CLI_BUILD=0
 export DISABLE_MAGIC_FUNCTIONS=true
 export DOCKER_BUILDKIT=0
 export HOMEBREW_NO_AUTO_UPDATE=1
 export HOMEBREW_NO_ENV_HINTS=1
 export HOMEBREW_NO_INSTALL_CLEANUP=1
-export ARCHPREFERENCE=arm64,x86_64
 # export ZSH_AUTOSUGGEST_MANUAL_REBIND=1  # make prompt faster
+
 # vim:ft=zsh:sw=2:sts=2
