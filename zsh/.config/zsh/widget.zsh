06466e23f64fbefe56f56e45210c2c51b4ec8e8a
vladislav doster <10052309+vladdoster@users.noreply.github.com>
2022-09-05 23:51:59 -0500

    fix: `_accept-line-with-url` widget
    Signed-off-by: vladislav doster <10052309+vladdoster@users.noreply.github.com>

--- zsh/.config/zsh/widget.zsh
+++ zsh/.config/zsh/widget.zsh
@@ -1,15 +1,20 @@
 #!/usr/bin/env zsh
 
-_accept-line-with-url() {
-  if  [[ $BUFFER =~ ^https.*git ]]; then
-    echo $BUFFER >> $HISTFILE
-    fc -R
-    BUFFERz="cd ${HOME} && git clone ${BUFFER} && cd $(basename ${BUFFER} .git)"
-    zle .kill-whole-line
-    BUFFER=${BUFFER}z
-    zle .accept-line
-  else
-    zle .accept-line
-  fi
+function _accept-line-with-url {
+
+    if  [[ $BUFFER =~ ^https.*git ]]
+    then
+
+        echo $BUFFER >> $HISTFILE
+        fc -R
+
+        BUFFERz="git clone $BUFFER && cd $(basename $BUFFER .git)"
+        zle .kill-whole-line
+        BUFFER=$BUFFERz
+        zle .accept-line
+    else 
+        zle .accept-line
+    fi 
 }
+
 zle -N accept-line _accept-line-with-url
