dbc9614bc44b60ab8afdf9fa5f356330ed35cb82
Vladislav Doster <mvdoster@gmail.com>
2022-10-06 19:52:56 -0500

    feat: add zsh precmd to fix broken term output
    Signed-off-by: Vladislav Doster <mvdoster@gmail.com>

--- zsh/.config/zsh/widget.zsh
+++ zsh/.config/zsh/widget.zsh
@@ -1,31 +1,29 @@
 #!/usr/bin/env zsh
 function _accept-line-with-url {
-    if  [[ $BUFFER =~ ^https.*git ]]
-    then
-        echo $BUFFER >> $HISTFILE
-        fc -R
-        BUFFERz="git clone $BUFFER && cd $(basename $BUFFER .git)"
-        zle .kill-whole-line
-        BUFFER=$BUFFERz
-        zle .accept-line
-      elif [[ $BUFFER =~ ^[[:space:]]?\$[[:space:]] ]]
-    then
-        echo $BUFFER >> $HISTFILE
-        fc -R
-	BUFFERz="$(echo ${BUFFER/\$/} | xargs)"
-        # zle .kill-whole-line
-        BUFFER=$BUFFERz
-        zle .accept-line
-    else
-        zle .accept-line
-    fi
+  if [[ $BUFFER =~ ^https.*git ]]; then
+    echo $BUFFER >> $HISTFILE
+    fc -R
+    BUFFERz="git clone $BUFFER && cd $(basename $BUFFER .git)"
+    zle .kill-whole-line
+    BUFFER=$BUFFERz
+    zle .accept-line
+  elif [[ $BUFFER =~ ^[[:space:]]?\$[[:space:]] ]]; then
+    echo $BUFFER >> $HISTFILE
+    fc -R
+    BUFFERz="$(echo ${BUFFER/\$/} | xargs)"
+    # zle .kill-whole-line
+    BUFFER=$BUFFERz
+    zle .accept-line
+  else
+    zle .accept-line
+  fi
 }
 zle -N accept-line _accept-line-with-url
 
-# Local Variables:
-# mode: Shell-Script
-# sh-indentation: 2
-# indent-tabs-mode: nil
-# sh-basic-offset: 2
-# End:
-# vim: ft=zsh sw=2 ts=2 et
+function reset_broken_terminal() {
+  printf '%b' '\e[0m\e(B\e)0\017\e[?5l\e7\e[0;0r\e8'
+}
+autoload -Uz add-zsh-hook
+add-zsh-hook -Uz precmd reset_broken_terminal
+
+# vim:ft=zsh:sw=2:sts=2
