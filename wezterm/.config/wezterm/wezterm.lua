696e79d84ac2eedb699a86edd1d7b7d6730371fe
Vladislav Doster <mvdoster@gmail.com>
2023-03-23 05:33:17 -0500

    fix: wezterm configuration
    Signed-off-by: Vladislav Doster <mvdoster@gmail.com>

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,35 +1,36 @@
 -- vim: set fenc=utf8 ffs=unix ft=zsh list et sts=2 sw=2 ts=2 tw=100:
-local wezterm = require('wezterm')
+local W = require('wezterm')
 return {
   adjust_window_size_when_changing_font_size=false,
   audible_bell='Disabled',
   check_for_updates=false,
-  color_scheme='SpaceGray',
+  color_scheme='Macintosh (base16)',
   default_prog={'zsh'},
   enable_csi_u_key_encoding=true,
   enable_scroll_bar=true,
   enable_tab_bar=true,
   exit_behavior='Close',
+  font=W.font('BlexMono Nerd Font Mono', { weight = 'Bold', italic = true }),
   font_size=18,
   force_reverse_video_cursor=true,
   hide_tab_bar_if_only_one_tab=true,
+  native_macos_fullscreen_mode=true,
+  quit_when_all_windows_are_closed=false,
   scrollback_lines=100000,
   show_update_window=true,
   unicode_version=14,
   use_dead_keys=false,
   use_fancy_tab_bar=true,
   window_close_confirmation='NeverPrompt',
-  color_scheme='Macintosh (base16)',
-  font=wezterm.font {
-    family='BlexMono Nerd Font Mono',
-    harfbuzz_features={'cv29', 'cv30', 'ss01', 'ss03', 'ss06', 'ss07', 'ss09'}
-  },
-  leader={key='a', mods='CTRL'},
-  keys={
-    {action=wezterm.action.SplitPane {direction='Down', size={Percent=50}}, mods='SUPER|SHIFT', key='Enter'},
-    {action=wezterm.action.SplitPane {direction='Right', size={Percent=50}}, mods='SUPER', key='Enter'},
-    {action=wezterm.action.ToggleFullScreen, mods='ALT|CTRL', key='f'},
-    {key='w', mods='SUPER', action=wezterm.action.CloseCurrentPane {confirm=true}}
+  window_padding={left='0.1cell', right='0.1cell', top='0.1cell', bottom='0.1cell'},
+  leader = { key = 'a', mods = 'CTRL', timeout_milliseconds = 1000 },
+  keys = {
+    {key = '|', mods = 'LEADER|SHIFT', action = W.action.SplitHorizontal { domain = 'CurrentPaneDomain' }},
+    {key = '-',mods = 'LEADER',action = W.action.SplitVertical { domain = 'CurrentPaneDomain' }},
+    {action=W.action.SplitPane {direction='Down', size={Percent=50}}, mods='SUPER|SHIFT', key='Enter'},
+    {action=W.action.SplitPane {direction='Right', size={Percent=50}}, mods='SUPER', key='Enter'},
+    {action=W.action.ToggleFullScreen, mods='ALT|CTRL', key='f'},
+    {key='w', mods='SUPER', action=W.action.CloseCurrentPane {confirm=true}}
   },
   window_frame={
     border_left_width='0.1cell',
@@ -39,8 +40,6 @@ return {
     border_left_color='red',
     border_right_color='red',
     border_bottom_color='red',
-    border_top_color='red'
+    border_top_color='red',
   },
-  window_padding={left='0.1cell', right='0.1cell', top='0.1cell', bottom='0.1cell'},
-  window_padding={bottom=0, left=0, right=0, top=0}
 }
