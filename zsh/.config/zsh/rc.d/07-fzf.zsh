42fe9f31439e8940bbddaefcc7cb8953c19466d1
Vladislav Doster <mvdoster@gmail.com>
2023-04-19 19:45:57 +0000

    fix: fzf config conditional checks
    Signed-off-by: Vladislav Doster <mvdoster@gmail.com>

--- zsh/.config/zsh/rc.d/07-fzf.zsh
+++ zsh/.config/zsh/rc.d/07-fzf.zsh
@@ -1,47 +1,51 @@
-_gen_fzf_default_opts() {
-  local background='#0f1117'
-  local bblack='#575860'
-  local bg3='#29394f'
-  local bg4='#39605d'
-  local black='#000000'
-  local blue='#719cd6'
-  local cyan='#63cdcf'
-  local foreground='#cdcecf'
-  local green='#81b29a'
-  local magenta='#9d79d6'
-  local red='#ff0000'
-  local white='#fdfefe'
-  local yellow='#f9e79f'
+(( ! $+commands[fzf] )) && {
+    _gen_fzf_default_opts() {
+      local background='#0f1117'
+      local bblack='#575860'
+      local bg3='#29394f'
+      local bg4='#39605d'
+      local black='#000000'
+      local blue='#719cd6'
+      local cyan='#63cdcf'
+      local foreground='#cdcecf'
+      local green='#81b29a'
+      local magenta='#9d79d6'
+      local red='#ff0000'
+      local white='#fdfefe'
+      local yellow='#f9e79f'
 
-  export FZF_DEFAULT_OPTS="
-    --color bg+:$bg3
-    --color bg:$background
-    --color border:$bg4
-    --color fg+:$yellow
-    --color fg:$bblack
-    --color gutter:$background
-    --color header:$white
-    --color hl+:$magenta
-    --color hl:$magenta
-    --color info:$yellow
-    --color marker:$blue
-    --color pointer:$white
-    --color prompt:$white
-    --color spinner:$green
-    --info inline
-    --layout reverse
-    --marker '+ '
-    --padding 2,5
-    --pointer '>'
-    --prompt '> '
-  "
-}
+      export FZF_DEFAULT_OPTS="
+        --color bg+:$bg3
+        --color bg:$background
+        --color border:$bg4
+        --color fg+:$yellow
+        --color fg:$bblack
+        --color gutter:$background
+        --color header:$white
+        --color hl+:$magenta
+        --color hl:$magenta
+        --color info:$yellow
+        --color marker:$blue
+        --color pointer:$white
+        --color prompt:$white
+        --color spinner:$green
+        --info inline
+        --layout reverse
+        --marker '+ '
+        --padding 2,5
+        --pointer '>'
+        --prompt '> '
+      "
+    }
 
-_gen_fzf_default_opts
+    _gen_fzf_default_opts
 
-export FZF_DEFAULT_COMMAND="rg --files --hidden --follow --glob '!.git'"
+    (( ! $+commands[rg] )) && {
+        export FZF_DEFAULT_COMMAND="rg --files --hidden --follow --glob '!.git'"
+    }
 
-[[ $- == *i* ]] && {
-    source "$(brew --prefix)/opt/fzf/shell/completion.zsh"
-    source "$(brew --prefix)/opt/fzf/shell/key-bindings.zsh"
+    if (( ! $+commands[brew] )) && [[ $- == *i* ]] {
+        source "$(brew --prefix)/opt/fzf/shell/completion.zsh"
+        source "$(brew --prefix)/opt/fzf/shell/key-bindings.zsh"
+    }
 }
