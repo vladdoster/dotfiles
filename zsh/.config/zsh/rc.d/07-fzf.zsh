690835b4d5d932d304ae01f2dee6a8ded70a2747
Vladislav Doster <mvdoster@gmail.com>
2023-05-31 21:29:24 -0500

    fix: fzf config logic checks
    Signed-off-by: Vladislav Doster <mvdoster@gmail.com>

--- zsh/.config/zsh/rc.d/07-fzf.zsh
+++ zsh/.config/zsh/rc.d/07-fzf.zsh
@@ -1,18 +1,18 @@
-(( ! $+commands[fzf] )) && {
-    _gen_fzf_default_opts() {
-      local background='#0f1117'
-      local bblack='#575860'
-      local bg3='#29394f'
-      local bg4='#39605d'
-      local black='#000000'
-      local blue='#719cd6'
-      local cyan='#63cdcf'
-      local foreground='#cdcecf'
-      local green='#81b29a'
-      local magenta='#9d79d6'
-      local red='#ff0000'
-      local white='#fdfefe'
-      local yellow='#f9e79f'
+if (( $+commands[fzf] )); then
+    () {
+      background='#0f1117'
+      bblack='#575860'
+      bg3='#29394f'
+      bg4='#39605d'
+      black='#000000'
+      blue='#719cd6'
+      cyan='#63cdcf'
+      foreground='#cdcecf'
+      green='#81b29a'
+      magenta='#9d79d6'
+      red='#ff0000'
+      white='#fdfefe'
+      yellow='#f9e79f'
 
       export FZF_DEFAULT_OPTS="
         --color bg+:$bg3
@@ -38,14 +38,12 @@
       "
     }
 
-    _gen_fzf_default_opts
+    if (( $+commands[rg] )); then
+        export FZF_DEFAULT_COMMAND="rg --files --follow--glob '!.git' --hidden"
+    fi
 
-    (( ! $+commands[rg] )) && {
-        export FZF_DEFAULT_COMMAND="rg --files --hidden --follow --glob '!.git'"
-    }
-
-    if (( ! $+commands[brew] )) && [[ $- == *i* ]] {
+    if (( $+commands[brew] )) && [[ $- == *i* ]]; then
         source "$(brew --prefix)/opt/fzf/shell/completion.zsh"
         source "$(brew --prefix)/opt/fzf/shell/key-bindings.zsh"
-    }
-}
+    fi
+fi
