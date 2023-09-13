399cfda79ff9e4a382c1fc31e599c57372699044
vladislav doster <10052309+vladdoster@users.noreply.github.com>
2023-09-12 20:46:17 -0500

    fix: wezterm open url mouse binding
    Signed-off-by: vladislav doster <10052309+vladdoster@users.noreply.github.com>

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,11 +1,25 @@
 local wezterm = require 'wezterm'
+local act = wezterm.action
 local mux = wezterm.mux
 local config = {}
 
 wezterm.on('gui-startup', function(cmd)
-  local tab, pane, window = mux.spawn_window(cmd or {})
+  local _, _, window = mux.spawn_window(cmd or {})
   window:gui_window():maximize()
 end)
+
+local mouse_bindings = {
+  { -- Click only selects text and doesn't open hyperlinks
+    event = { Up = { streak = 1, button = 'Left' } },
+    mods = 'NONE',
+    action = act.CompleteSelection 'ClipboardAndPrimarySelection',
+  },
+  { -- CTRL-Click open hyperlinks
+    event = { Up = { streak = 1, button = 'Left' } },
+    mods = 'CTRL',
+    action = act.OpenLinkAtMouseCursor,
+  },
+}
 
 config = {
   adjust_window_size_when_changing_font_size=false,
@@ -17,7 +31,7 @@ config = {
   enable_scroll_bar=true,
   enable_tab_bar=true,
   exit_behavior='Close',
-  font=wezterm.font('BlexMono Nerd Font Mono Complete', { weight = 'Text', italic = true }),
+  font=wezterm.font('BlexMono Nerd Font Mono'),
   font_size=18,
   force_reverse_video_cursor=true,
   hide_tab_bar_if_only_one_tab=true,
@@ -25,12 +39,15 @@ config = {
   quit_when_all_windows_are_closed=false,
   scrollback_lines=100000,
   show_update_window=true,
+  pane_focus_follows_mouse = true,
   unicode_version=14,
   use_dead_keys=false,
+  use_resize_increments = true,
   use_fancy_tab_bar=true,
   window_close_confirmation='NeverPrompt',
   window_padding={left='0.1cell', right='0.1cell', top='0.1cell', bottom='0.1cell'},
   leader = { key = 'a', mods = 'CTRL', timeout_milliseconds = 1000 },
+  mouse_bindings = mouse_bindings,
   keys = {
     {key = '|', mods = 'LEADER|SHIFT', action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' }},
     {key = '-',mods = 'LEADER',action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' }},
