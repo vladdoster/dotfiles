2ddbef4e59ca5d12e3db9e861eb24fe9b9bcaf8d
Vladislav Doster <mvdoster@gmail.com>
2023-12-03 23:01:10 -0600

    fix: remove aliases
    Signed-off-by: Vladislav Doster <mvdoster@gmail.com>

--- zsh/.config/zsh/rc.d/01-alias.zsh
+++ zsh/.config/zsh/rc.d/01-alias.zsh
@@ -82,13 +82,10 @@ for k v in hscfg '.hammerspoon/init.lua' sshrc '.ssh/config' zec '.zshenv' zpc '
     builtin alias -- $k="_edit ${HOME}/${v}" || true
 done
 
-alias nvcln='command rm -rf $HOME/.{local/share/nvim,config/nvim/plugin/packer_compiled.lua}'
-alias zicln='command rm -rf ${HOME}/.{local/share/{zinit,zsh},cache,config/{zinit,zsh/.{zcomp{cache,dump},zsh_sessions}}}'
-alias ziprune='zi delete --all --yes; ( exec zsh -il );'
 alias zrld='builtin exec zsh -i'
 alias zireset='builtin cd ${HOME}; unset _comp{_{assocs,dumpfile,options,setup},{auto,}s}; ziprune; zrld; cd -'
 
-typeset -A pairs=(.. '../' ... '../../' .... '../../../' bin '~/.local/bin' dl '~/Downloads' hsd '~/.hammerspoon' xch '~/.config' xdh '~/.local/share' zcf '$ZDOTDIR/rc.d' df '${DOTFILES:-~/.config/dotfiles}') 
+typeset -A pairs=(bin '~/.local/bin' dl '~/Downloads' hsd '~/.hammerspoon' xch '~/.config' xdh '~/.local/share' zcf '$ZDOTDIR/rc.d' df '${DOTFILES:-~/.config/dotfiles}') 
 for k v in ${(kv)pairs[@]}; do
     builtin alias -- "$k"="_goto $v" || true
 done
@@ -98,8 +95,6 @@ for k v in g '\git' gd '\git diff' gs '\git status' gsu '\git submodule update -
 done
 
 alias auld='builtin autoload'
-alias me='builtin print -P "%F{blue}$(whoami)%f @ %F{cyan}$(uname -a)%f"'
-alias mk='make'
 
 alias zc='zinit compile'
 alias zgd='cd $ZINIT[BIN_DIR]; ls'
@@ -107,36 +102,19 @@ alias zhd='cd $ZINIT[HOME_DIR]; ls'
 alias zht='hyperfine --warmup 100 --runs 10000 "/bin/ls"'
 alias zmld="builtin zmodload"
 
-alias -- +x='chmod +x'
-alias -- \?='which'
 alias gen-passwd='openssl rand -base64 24'
 
 alias get-my-ip='curl ifconfig.co'
 alias get-env='print -lio $(env)'
-alias get-path='print -l ${(@s[:])PATH}'
 
-alias tmp-md='$EDITOR $(mktemp -t scratch.XXX.md)'
 alias git-repo-website='open $(git remote get-url origin)'
 alias http-serve='python3 -m http.server'
 alias get-localnet-hosts='sudo arp-scan --format="\${Name;-30}\${ip}" --localnet --ignoredups --plain --resolve | sort --human-numeric-sort'
 alias get-open-ports='sudo lsof -i -n -P | grep TCP'
 alias ping='ping -c 10'
 
-alias mkcd='{ local DIR_NAME="$(cat -)"; command mkdir -p -- "$DIR_NAME" && builtin cd -P -- $DIR_NAME }<<<'
-alias mkcmd='{ F_NAME="$(cat -)"; touch "$F_NAME"; chmod +x $F_NAME; rehash; nvim $F_NAME }<<<'
-alias mkmd='{ F_NAME="$(cat -).md"; touch "$F_NAME"; _info "created: $F_NAME"; }<<<'
-alias mkpy='_mkfile py "python3"'
-alias mksh='_mkfile sh "bash"'
-alias mktxt='{ F_NAME="$(cat -).txt"; touch "$F_NAME"; _info "created: $F_NAME"; }<<<'
-alias mkzsh='_mkfile zsh "zsh"'
-
 alias fmtbtysh='python3 -m beautysh --indent-size=2 --force-function-style=paronly'
 alias fmtlua='stylua -i'
 alias fmtmd='mdformat --number --wrap 100'
 alias fmtpy='python3 -m black'
 alias fmtsh='shfmt -bn -ci -i 2 -ln=bash -s -sr -w'
-alias wsys='echo OSTYPE=${OSTYPE} MACHTYPE=${MACHTYPE} CPUTYPE=${CPUTYPE} hardware=$(uname -m) processor=$(uname -p)'
-
-alias cp-dotfiles='rsync -azP $XDG_CONFIG_HOME/dotfiles/ devcloud:~/dotfiles'
-alias cp-hammerspoon='rsync -azP $HOME/.hammerspoon/ devcloud:~/hammerspoon'
-alias cp-nvim='rsync -azP $XDG_CONFIG_HOME/nvim/ devcloud:~/nvim'
