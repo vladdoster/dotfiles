1e90d3a9eebccd8ed258fc4b874c1c5cda3a0778
Vladislav Doster <mvdoster@gmail.com>
2024-03-15 02:13:13 -0500

    refactor(rc.d/fzf): store config in array instead of multi-line string
    Signed-off-by: Vladislav Doster <mvdoster@gmail.com>

--- zsh/.config/zsh/rc.d/03-fzf.zsh
+++ zsh/.config/zsh/rc.d/03-fzf.zsh
@@ -1,28 +1,21 @@
 #!/usr/bin/env zsh
 
 if (( $+commands[fzf] )); then
-  () {
-    local \
-      background='#0f1117' bblack='#575860'  bg3='#29394f'  bg4='#39605d'        \
-      black='#000000'      blue='#719cd6'    cyan='#63cdcf' foreground='#cdcecf' \
-      green='#81b29a'      magenta='#9d79d6' red='#ff0000'  white='#fdfefe'      yellow='#f9e79f'
-    export FZF_DEFAULT_OPTS="
-      --color bg+:$bg3 --color bg:$background
-      --color border:$bg4
-      --color fg+:$yellow --color fg:$bblack
-      --color gutter:$background
-      --color header:$white
-      --color hl+:$magenta --color hl:$magenta
-      --color info:$yellow
-      --color marker:$blue
-      --color pointer:$white
-      --color prompt:$white
-      --color spinner:$green
-      --info inline
-      --layout reverse
-      --marker '+ '
-      --padding 2,5
-      --pointer '>'
-      --prompt '> '" 
-  }
+    local -A theme=(
+        background '#0f1117' bblack '#575860' bg3 '#29394f'
+        bg4 '#39605d' black '#000000' blue '#719cd6'
+        cyan '#63cdcf' foreground '#cdcecf' green '#81b29a'
+        magenta '#9d79d6' red '#ff0000' white '#fdfefe'
+        yellow '#f9e79f'
+    )
+    local -a fzf_default_opts=(
+        "--color fg:${theme[bblack]} --color bg+:${theme[bg3]} --color bg:${theme[background]}"
+        "--color border:${theme[bg4]} --color fg+:${theme[yellow]} --color gutter:${theme[background]}"
+        "--color header:${theme[white]} --color hl+:${theme[magenta]} --color hl:${theme[magenta]}"
+        "--color info:${theme[yellow]} --color marker:${theme[blue]} --color pointer:${theme[white]}"
+        "--color prompt:${theme[white]} --color spinner:${theme[green]}"
+        "--info inline --layout reverse"
+        "--marker '+ ' --padding 2,5 --pointer '>' --prompt '>'"
+    )
+    export FZF_DEFAULT_OPTS=${^fzf_default_opts}
 fi
