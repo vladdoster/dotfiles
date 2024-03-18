b496afdf4a72b947c6bf70624c1ec6328398c3c2
Vladislav Doster <mvdoster@gmail.com>
2024-03-18 07:56:29 -0500

    fix(rc.d/00-env): python condfig & format file
    Signed-off-by: Vladislav Doster <mvdoster@gmail.com>

--- zsh/.config/zsh/rc.d/00-env.zsh
+++ zsh/.config/zsh/rc.d/00-env.zsh
@@ -1,4 +1,6 @@
 #!/usr/bin/env zsh
+#  vim: set expandtab filetype=zsh shiftwidth=2 tabstop=2 :
+zsh_load_info
 # environment variables
 (( ${+TERM} )) || export TERM="xterm-256color"; COLORTERM="truecolor"
 (( ${+USER} )) || export USER="${USERNAME}"
@@ -7,26 +9,24 @@
 (( ${+XDG_DATA_HOME} )) || export XDG_DATA_HOME="${HOME}/.local/share"
 # configuration directories
 export \
-  CODEDIR="$HOME/code" DOTFILES="${XDG_CONFIG_HOME}/dotfiles" \
-  GIT_CONFIG="${XDG_CONFIG_HOME}/git/config" PIP_CONFIG="${XDG_CONFIG_HOME}/pip" \
-  PYTHONPATH="${XDG_DATA_HOME}/python" VIMDOTDIR="${XDG_CONFIG_HOME}/vim" \
+  CODEDIR="$HOME/code" \
+  DOTFILES="${XDG_CONFIG_HOME}/dotfiles"     \
+  GIT_CONFIG="${XDG_CONFIG_HOME}/git/config" \
+  PIP_CONFIG="${XDG_CONFIG_HOME}/pip"        \
+  VIMDOTDIR="${XDG_CONFIG_HOME}/vim"         \
   ZDOTDIR="${ZDOTDIR:-$HOME/.config/zsh}"
+  # PYTHONPATH="${XDG_DATA_HOME}/python"
 # program options
 export \
-  COMPOSE_DOCKER_CLI_BUILD=1 CORRECT_IGNORE="*zinit[-]*" \
-  DISABLE_MAGIC_FUNCTIONS=1  DOCKER_BUILDKIT=1 \
-  HOMEBREW_NO_{ENV_HINTS,INSTALL_CLEANUP}=1 \
+  COMPOSE_DOCKER_CLI_BUILD=1  \
+  CORRECT_IGNORE="*zinit[-]*" \
+  DISABLE_MAGIC_FUNCTIONS=1   \
+  DOCKER_BUILDKIT=1           \
+  HOMEBREW_NO_{'ENV_HINTS','INSTALL_CLEANUP'}=1 \
   SHELL_SESSIONS_DISABLE=1
 
-export PIP_TRUSTED_HOST='files.pythonhosted.org pypi.org'
-export PIP_UPGRADE='true'
-export PIP_NO_CACHE_DIR='true'
-export PIP_TARGET="$HOME/.local/share/python"
-
-#------------------------------
-# Prompt
-#------------------------------
-autoload -U colors zsh/terminfo
-colors
-
-#  vim: set expandtab filetype=zsh shiftwidth=4 tabstop=4 :
+export \
+  PIP_TRUSTED_HOST='files.pythonhosted.org pypi.org' \
+  PIP_UPGRADE='true' \
+  PIP_NO_CACHE_DIR='true'
+# export PIP_TARGET="$HOME/.local/share/python"
