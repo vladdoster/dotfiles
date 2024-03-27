3f7f058413996f2e88922a8f00c6d7afed572550
Vladislav Doster <mvdoster@gmail.com>
2024-03-26 23:23:29 -0500

    feat(zsh): print alias when ran via widget
    To enable, run: `enable_print_alias`
    To disable, run: `disable_print_alias`
    
    Signed-off-by: Vladislav Doster <mvdoster@gmail.com>

--- zsh/.config/zsh/rc.d/02-widget.zsh
+++ zsh/.config/zsh/rc.d/02-widget.zsh
@@ -51,4 +51,53 @@ function reset_broken_terminal() {
 autoload -Uz add-zsh-hook
 add-zsh-hook -Uz precmd reset_broken_terminal
 
+local cmd_alias=""
+
+# ---------------------
+# Reveal Executed Alias
+# ---------------------
+# Note: disable by removing it from the hook.
+# add-zsh-hook -d preexec pre_validation
+alias_for() {
+  [[ $1 =~ '[[:punct:]]' ]] && return
+  local search=${1}
+  local found="$( alias $search )"
+  if [[ -n $found ]]; then
+    found=${found//\\//}          # replace backslash with slash
+    found=${found%\'}             # remove end single quote
+    found=${found#"$search="}     # remove alias name
+    found=${found#"'"}            # remove first single quote
+    echo "${found} ${2}" | xargs  # return found value (with parameters)
+  else
+    echo ""
+  fi
+}
+
+expand_command_line() {
+  first=$(echo "$1" | awk '{print $1;}')
+  rest=$(echo ${${1}/"${first}"/})
+
+  if [[ -n "${first//-//}" ]]; then
+    cmd_alias="$(alias_for "${first}" "${rest:1}")"                   # check if there's an alias for the command
+    if [[ -n $cmd_alias ]] && [[ "${cmd_alias:0:1}" != "." ]]; then   # if there was and not start with dot
+      echo "${T_GREEN}ᐳ ${T_YELLOW}${cmd_alias}${F_RESET}"            # otherwise print it
+    fi
+  fi
+}
+
+pre_validation() {
+  [[ $# -eq 0 ]] && return
+  expand_command_line "$@"
+}
+
+function enable_print_alias () {
+    autoload -U add-zsh-hook    # Load the zsh hook module. 
+    add-zsh-hook preexec pre_validation
+}
+
+
+function disable_print_alias () {
+  add-zsh-hook -d preexec pre_validation
+}
+
 # vim: set expandtab filetype=zsh shiftwidth=2 softtabstop=2 tabstop=2:
