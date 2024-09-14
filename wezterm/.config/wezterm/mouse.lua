fb8a882af17349d78d5d57d273b84d3a3a1722f0
Vladislav Doster <mvdoster@gmail.com>
2024-09-14 11:19:27 -0500

    style(wezterm): disable ligatures & format via stylua
    Stylua command:
    
    ```bash
    stylua \
      --call-parentheses Always \
      --collapse-simple-statement Never \
      --column-width 100 \
      --glob **/*.lua \
      --indent-type Spaces \
      --indent-width 2 \
      --line-endings Unix \
      --no-editorconfig \
      --quote-style AutoPreferDouble \
      --sort-requires \
      --verbose
    ```
    
    Signed-off-by: Vladislav Doster <mvdoster@gmail.com>

--- wezterm/.config/wezterm/mouse.lua
+++ wezterm/.config/wezterm/mouse.lua
@@ -1,4 +1,4 @@
-local wezterm = require('wezterm')
+local wezterm = require("wezterm")
 -- local keys = require("keys")
 
 local M = {}
@@ -6,17 +6,19 @@ local M = {}
 ---@param config Config
 function M.setup(config)
   config.alternate_buffer_wheel_scroll_speed = 1
-  config.bypass_mouse_reporting_modifiers = 'CTRL'
+  config.bypass_mouse_reporting_modifiers = "CTRL"
   config.mouse_bindings = {
     -- Don't open links without modifier
     {
-      event = { Up = { streak = 1, button = 'Left' } },
-      action = wezterm.action.CompleteSelection('ClipboardAndPrimarySelection'),
+      event = { Up = { streak = 1, button = "Left" } },
+      action = wezterm.action.CompleteSelection("ClipboardAndPrimarySelection"),
     },
     {
-      event = { Up = { streak = 1, button = 'Left' } },
-      mods = 'CTRL',
-      action = wezterm.action.CompleteSelectionOrOpenLinkAtMouseCursor('ClipboardAndPrimarySelection'),
+      event = { Up = { streak = 1, button = "Left" } },
+      mods = "CTRL",
+      action = wezterm.action.CompleteSelectionOrOpenLinkAtMouseCursor(
+        "ClipboardAndPrimarySelection"
+      ),
     },
   }
 end
--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,27 +1,29 @@
-local wezterm = require('wezterm')
+local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
-require('links').setup(config)
-require('mouse').setup(config)
+require("links").setup(config)
+require("mouse").setup(config)
 
 local act = wezterm.action
 local mux = wezterm.mux
 
-wezterm.on('gui-startup', function(cmd)
+wezterm.on("gui-startup", function(cmd)
   local _, _, window = mux.spawn_window(cmd or {})
   window:gui_window():maximize()
 end)
 
 config = {
   adjust_window_size_when_changing_font_size = false,
-  audible_bell = 'Disabled',
+  audible_bell = "Disabled",
   check_for_updates = false,
-  color_scheme = 'Windows High Contrast (base16)', -- 'Windows NT (base16)'
+  color_scheme = "Windows High Contrast (base16)", -- 'Windows NT (base16)'
   enable_csi_u_key_encoding = true,
   enable_scroll_bar = true,
   enable_tab_bar = true,
-  exit_behavior = 'Close',
-  font = wezterm.font('BlexMono Nerd Font Mono'),
+  exit_behavior = "Close",
+  harfbuzz_features = { "calt=0", "clig=0", "liga=0" },
+  font = wezterm.font("Server Mono"),
+  -- font = wezterm.font('BlexMono Nerd Font Mono'),
   font_size = 22,
   force_reverse_video_cursor = true,
   hide_tab_bar_if_only_one_tab = true,
@@ -37,42 +39,42 @@ config = {
   use_resize_increments = true,
   keys = {
     {
-      action = wezterm.action.SplitPane { direction = 'Down', size = { Percent = 50 } },
-      mods = 'SUPER|SHIFT',
-      key = 'Enter',
+      action = wezterm.action.SplitPane({ direction = "Down", size = { Percent = 50 } }),
+      key = "Enter",
+      mods = "SUPER|SHIFT",
     },
     {
-      action = wezterm.action.SplitPane { direction = 'Right', size = { Percent = 50 } },
-      mods = 'SUPER',
-      key = 'Enter',
+      action = wezterm.action.SplitPane({ direction = "Right", size = { Percent = 50 } }),
+      key = "Enter",
+      mods = "SUPER",
     },
-    { action = wezterm.action.ToggleFullScreen, mods = 'ALT|CTRL', key = 'f' },
-    { key = 'w', mods = 'SUPER', action = wezterm.action.CloseCurrentPane { confirm = true } },
-    { key = 'l', mods = 'ALT', action = wezterm.action.ShowLauncher },
+    { action = wezterm.action.ToggleFullScreen, key = "f", mods = "ALT|CTRL" },
+    { action = wezterm.action.CloseCurrentPane({ confirm = true }), key = "w", mods = "SUPER" },
+    { action = wezterm.action.ShowLauncher, key = "l", mods = "ALT" },
   },
   mouse_bindings = {
     { -- Click only selects text and doesn't open hyperlinks
-      event = { Up = { streak = 1, button = 'Left' } },
-      mods = 'NONE',
-      action = act.CompleteSelection 'ClipboardAndPrimarySelection',
+      action = act.CompleteSelection("ClipboardAndPrimarySelection"),
+      event = { Up = { streak = 1, button = "Left" } },
+      mods = "NONE",
     },
     { -- CTRL-Click open hyperlinks
-      event = { Up = { streak = 1, button = 'Left' } },
-      mods = 'CTRL',
       action = act.OpenLinkAtMouseCursor,
+      event = { Up = { streak = 1, button = "Left" } },
+      mods = "CTRL",
     },
   },
-  window_close_confirmation = 'NeverPrompt',
-  window_padding = { left = '0.1cell', right = '0.1cell', top = '0.1cell', bottom = '0.1cell' },
+  window_close_confirmation = "NeverPrompt",
+  window_padding = { bottom = "0.1cell", left = "0.1cell", right = "0.1cell", top = "0.1cell" },
   window_frame = {
-    border_bottom_color = 'red',
-    border_bottom_height = '0.1cell',
-    border_left_color = 'red',
-    border_left_width = '0.1cell',
-    border_right_color = 'red',
-    border_right_width = '0.1cell',
-    border_top_color = 'red',
-    border_top_height = '0.1cell',
+    border_bottom_color = "red",
+    border_bottom_height = "0.1cell",
+    border_left_color = "red",
+    border_left_width = "0.1cell",
+    border_right_color = "red",
+    border_right_width = "0.1cell",
+    border_top_color = "red",
+    border_top_height = "0.1cell",
   },
 }
 
