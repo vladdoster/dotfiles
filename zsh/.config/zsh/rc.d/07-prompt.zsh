70288148f7af123a9dbb6147807c4c2d0b00b8f6
Vladislav Doster <mvdoster@gmail.com>
2026-08-27 23:46:15 -0500

    refactor(zsh): remove unused prompt code and fix precmd hook
    Drop the never-invoked prompt_minimal_setup system, its
    +vi-git-status hook, and commented-out prompt experiments.
    
    Register vcs_info via add-zsh-hook instead of defining precmd
    directly, which clobbered hooks added by zinit, autosuggestions,
    and vim-mode. Limit vcs_info backends to git.
    
    Signed-off-by: Vladislav Doster <mvdoster@gmail.com>

--- zsh/.config/zsh/rc.d/07-prompt.zsh
+++ zsh/.config/zsh/rc.d/07-prompt.zsh
@@ -1,37 +1,20 @@
-function +vi-git-status() {
-  # check for untracked files or updated submodules since vcs_info does not
-  if [[ -n $(git ls-files --other --exclude-standard 2> /dev/null) ]]; then
-    hook_com[unstaged]='%F{yellow}~%f'
-  fi
-}
+setopt prompt_subst
 
-function prompt_minimal_precmd {
-  vcs_info
-}
+autoload -Uz add-zsh-hook vcs_info
+add-zsh-hook precmd vcs_info
 
-function prompt_minimal_setup {
-  setopt LOCAL_OPTIONS
-  unsetopt XTRACE KSH_ARRAYS
-  prompt_opts=(cr percent subst)
-  # load required functions
-  autoload -Uz add-zsh-hook
-  autoload -Uz vcs_info
-  # add hook for calling vcs_info before each command
-  add-zsh-hook precmd prompt_minimal_precmd
-  # set vcs_info parameters
-  zstyle ':vcs_info:*' enable bzr git hg svn
-  zstyle ':vcs_info:*' check-for-changes true
-  zstyle ':vcs_info:*' stagedstr '|%F{green}%b%f'
-  zstyle ':vcs_info:*' unstagedstr '%F{yellow}~%f'
-  zstyle ':vcs_info:*' formats '[%b|%u]'
-  zstyle ':vcs_info:*' actionformats "[%b%c%u|%F{cyan}%a%f]"
-  zstyle ':vcs_info:(sv[nk]|bzr):*' branchformat '%b|%F{cyan}%r%f'
-  zstyle ':vcs_info:git*+set-message:*' hooks git-status
-  # define prompts
-  PROMPT='%B%F{240}%1~%f%b ${vcs_info_msg_0_} ᐳ '
-  RPROMPT=''
+# Only check for git repositories
+zstyle ':vcs_info:*' enable git
 
-}
+# Check repositories for unstaged and staged changes
+zstyle ':vcs_info:*' check-for-changes true
 
-typeset -g PROMPT4='%B[%b%F{yellow}%N%f | %F{blue}%I%f%B]%b ᐳ'
-prompt_minimal_setup "$@"
+# Define strings for changes (%u = unstaged, %c = staged)
+zstyle ':vcs_info:*' unstagedstr '%F{red}*%f'
+zstyle ':vcs_info:*' stagedstr '%F{green}+%f'
+
+# Format string: (branch * +)
+zstyle ':vcs_info:git:*' formats '[%b%u%c] '
+zstyle ':vcs_info:git:*' actionformats '[%b|%a%u%c] '
+
+PROMPT='%B%F{cyan}%2~%f%b ${vcs_info_msg_0_}$ '
